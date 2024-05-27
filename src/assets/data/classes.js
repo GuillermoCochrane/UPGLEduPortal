80cb879d71e3fa2676c2a6c96b6e1c6012579c9b
GuillermoCochrane <guilleac81@gmail.com>
2024-05-27 19:40:36 -0300

    Creacion de la DB Classes

---
+++ src/assets/data/classes.js
@@ -0,0 +1,105 @@
+let classes =  [
+    {
+      id: 1,
+      classData: [
+                {
+                  type: "h2",
+                  info: ["Inteligencia Artificial, conceptos fundamentales"]
+                },
+                {
+                  type: "h3",
+                  info: ["¿Qué es la inteligencia?"]
+                },
+                {
+                  type: "p",
+                  info: ["La inteligencia se ha definido de muchas maneras, incluyendo: la capacidad de lógica, comprensión, autoconciencia, aprendizaje, conocimiento emocional, razonamiento, planificación, creatividad, pensamiento crítico y resolución de problemas. En términos más generales, se puede describir como la capacidad de percibir o inferir información, y retenerla como conocimiento para aplicarlo a comportamientos adaptativos dentro de un entorno o contexto."]
+                },
+                {
+                  type: "ul", 
+                  info: [
+                    {
+                      type: "li",
+                      info: [{text: "Ch. Spearman: ", bold: true},"Capacidad unitaria para resolver problemas y crear nuevos contenidos."]
+                    },
+                    {
+                      type: "li",
+                      info: [{text: "L. G. Humphreys: ", bold: true}, "Conjunto de habilidades para adaptarse al entorno."]
+                    },
+                    { 
+                      type: "li",        
+                      info: [{text: "L. S. Gottfredson: ", bold: true}, " Capacidad mental general que incluye la habilidad de razonar, planificar, resolver problemas, pensar en abstracto, comprender ideas complejas, aprender rápido y aprender de la experiencia, que es más que una destreza académica o del aprendizaje por medio de libros."]
+                    }
+                  ] 
+                },
+                {
+                  type: "h3",
+                  info: ["¿Qué es la inteligencia artificial?"]
+                },
+                {
+                  type: "p",
+                  info: ["La Inteligencia Artificial se refiere a la capacidad de las máquinas para realizar tareas que normalmente requieren inteligencia humana. Esto incluye el aprendizaje, el razonamiento, la percepción, la comprensión del lenguaje natural y la toma de decisiones. En esencia, la IA busca imitar las capacidades cognitivas humanas utilizando algoritmos y datos."]
+                },  
+                {
+                  type: "ol", 
+                  info: [
+                    {
+                      type: "li",
+                      info: [{text:"Aprendizaje Automático (Machine Learning): ", bold: true}, "Es una subdisciplina de la IA que se centra en el desarrollo de algoritmos que permiten a las máquinas aprender patrones y tomar decisiones basadas en datos sin una programación explícita."]
+                    },
+                    {
+                      type: "li",
+                      info: [{text: "Redes Neuronales Artificiales: ", bold: true}, "Inspiradas en la estructura del cerebro humano, las redes neuronales artificiales son modelos computacionales que pueden aprender y realizar tareas complejas como reconocimiento de patrones y procesamiento de lenguaje natural."]
+                    },        
+                  ]
+                },
+                {
+                  type: "h3",
+                  info: ["Diferencias entre Inteligencia Artificial e Inteligencia Natural"]
+                },
+                {
+                  type: "p",
+                  info: ["La principal diferencia entre la inteligencia artificial y la inteligencia natural radica en su origen y naturaleza."]
+                },  
+                {
+                  type: "h3",
+                  info: ["Origen"]
+                },  
+                {
+                  type: "ul", 
+                  info: [
+                    {
+                      type: "li",
+                      info: [{text: "Inteligencia Artificial: ", bold: true}, "Es creada por humanos a través del diseño y la programación de sistemas informáticos."]
+                    },
+                    {
+                      type: "li",
+                      info: [{text: "Inteligencia Natural: ", bold: true}, "Es inherente a los seres vivos, especialmente a los humanos, y se desarrolla a través de procesos biológicos y experiencia."]
+                    },
+                  ]
+                },
+                {
+                  type: "h3",
+                  info: ["Naturaleza"]
+                },  
+                {
+                  type: "ul", 
+                  info: [
+                    {
+                      type: "li",
+                      info: [{text: "Inteligencia Artificial: ", bold: true}, "Se basa en algoritmos y datos para imitar el comportamiento humano en tareas específicas. A menudo es especializada y puede carecer de comprensión profunda o conciencia."]
+                    },
+                    {
+                      type: "li",
+                      info: [{text: "Inteligencia Natural: ", bold: true}, "Es inherentemente compleja y abarca una amplia gama de habilidades cognitivas, emocionales y sociales. Se desarrolla a lo largo del tiempo a través de la interacción con el entorno y la experiencia."]
+                    },
+                  ]
+                }
+          ]
+    },
+    {
+      id: 2,
+      classData: [  ]
+    }
+  ]
+
+export default classes;
