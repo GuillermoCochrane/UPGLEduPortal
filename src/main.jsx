07960bedcfdf034b7cbb95cef044083ec2d21159
GuillermoCochrane <guilleac81@gmail.com>
2024-05-29 02:48:54 -0300

    Modificados Estilos y favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import App from "./components/App"
-import './index.css'
 import {BrowserRouter} from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
