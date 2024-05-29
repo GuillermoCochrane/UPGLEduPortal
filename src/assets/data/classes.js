0400aa04dc17af9ac34717187d809ef335541045
GuillermoCochrane <guilleac81@gmail.com>
2024-05-29 03:24:33 -0300

    Verificada clase 2

--- src/assets/data/classes.js
+++ src/assets/data/classes.js
@@ -309,7 +309,7 @@ let classes = [
       },
       {
         type: "h3",
-        info: [[{ text: "Década de 1980", content: "plain" }]],
+        info: [{ text: "Década de 1980", content: "plain" }],
       },
       {
         type: "p",
@@ -455,6 +455,25 @@ let classes = [
       },
       {
         type: "h3",
+        info: [{ text: "Década de 1990", content: "plain" }],
+      },
+      {
+        type: "p",
+        info: [ { text: "Durante la década de 1990, el enfoque en el aprendizaje automático y las redes neuronales resurgió con el desarrollo de algoritmos como las máquinas de vectores de soporte (SVM) y las redes neuronales convolucionales (CNN). Estos avances sentaron las bases para una nueva ola de aplicaciones de IA en áreas como la minería de datos, la detección de fraudes y el reconocimiento de patrones. ", content: "plain" }],
+      },
+      {
+        type: "p",
+        info: [
+          { text: "En ", content: "plain" },
+          { text: "1997", content: "bold" },
+          {
+            text: " la historia de la IA estuvo marcada por un acontecimiento importante. La IA Deep Blue de IBM triunfó sobre el campeón mundial de ajedrez Garry Kasparov. Por primera vez, el hombre fue derrotado por la máquina",
+            content: "plain",
+          },
+        ],  
+      },
+      {
+        type: "h3",
         info: [{ text: "Década de 2000", content: "plain" }],
       },
       {
@@ -518,12 +537,11 @@ let classes = [
       },
       {
         type: "figure",
-        info: [
+        info: 
           {
             img: "/images/timeline.png",
             alt: "Linea de tiempo de la historia de la IA",
           },
-        ],
       },
       {
         type: "h3",
