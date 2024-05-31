4b29669c6d69121e03893a35e3fc8c80dd405a3e
GuillermoCochrane <guilleac81@gmail.com>
2024-05-31 16:49:21 -0300

    Agregada data clase 5

--- src/assets/data/classes.js
+++ src/assets/data/classes.js
@@ -972,7 +972,133 @@ let classes = [
         ],
       },
     ]
-  }
+  },
+  {
+    id: 5,
+    classData: [
+      {
+        type: "h2",
+        info: [{ text: "Prompt: definición y conceptos", content: "plain" }],
+    
+      },
+      {
+        type: "h3",
+        info: [{ text: "¿Qué es un Prompt?", content: "plain" }], 
+      },
+      {
+        type: "p",
+        info: [
+          {
+            text: "En el ámbito de la ingeniería de Prompt, los \"Prompts\" son instrucciones o guiones que se proporcionan a modelos de lenguaje, como GPT (Generative Pre-trained Transformer), para guiar su generación de texto en una dirección específica. Estas instrucciones pueden variar en complejidad y longitud, desde simples palabras clave hasta párrafos detallados. La eficacia de un prompt radica en su capacidad para comunicar claramente la tarea deseada al modelo de lenguaje.", 
+            content: "plain",
+          },
+        ],
+      },
+      {
+        type: "h3",
+        info: [{ text: "Componentes de un Prompt", content: "plain" }],
+      },
+      {
+        type: "ul",
+        info: [
+          {
+            type: "li", 
+            info: [ 
+              { text: "Instrucciones Claras:  ", content: "bold" },
+              {   text: "Deben ser lo suficientemente claras para comunicar la tarea deseada al modelo de lenguaje.", content: "plain" },
+            ],
+          },
+          {
+            type: "li", 
+            info: [ 
+              { text: "Contexto Relevante:  ", content: "bold" },
+              {   text: "Proporcionar contexto relevante puede ayudar al modelo a comprender mejor la solicitud y generar una respuesta más precisa.", content: "plain" },
+            ],
+          },
+          {
+            type: "li", 
+            info: [ 
+              { text: "Palabras Clave:  ", content: "bold" },
+              {   text: "Incluir palabras clave relacionadas con la tarea puede orientar al modelo hacia el tipo de respuesta deseada.", content: "plain" },
+            ],
+          },
+          {
+            type: "li", 
+            info: [ 
+              { text: "Restricciones:  ", content: "bold" },
+              {   text: "Pueden incluir restricciones específicas sobre el estilo, tono o longitud de la respuesta.", content: "plain" },
+            ],
+          },
+          {
+            type: "li", 
+            info: [ 
+              { text: "Ejemplos:  ", content: "bold" },
+              {   text: "A veces, se pueden incluir ejemplos de respuestas deseadas para ayudar al modelo a comprender mejor la tarea.", content: "plain" },
+            ],
+          },
+        ],
+      },
+      {
+        type: "h3", 
+        info: [{ text: "Aplicación de Prompt en tareas especificas", content: "plain" }],
+      },
+      {
+        type: "p", 
+        info: [ 
+          { text: "Los prompts se utilizan en una variedad de tareas específicas en el campo de la ingeniería de prompt. Algunos ejemplos incluyen:", content: "plain" },
+        ],
+      },
+      {
+        type: "ul", 
+        info: [ 
+          {
+            type: "li", 
+            info: [ 
+              { text: "Generación de Texto Creativo:  ", content: "bold" },
+              { text: "Los prompts se utilizan para guiar la generación de textos creativos, como historias, poemas o diálogos. Por ejemplo, un prompt para generar un poema podría ser: \"Escribe un poema sobre la belleza de la naturaleza\".", content: "plain" },
+            ],
+          },
+          {
+            type: "li", 
+            info: [ 
+              { text: "Traducción Automática:  ", content: "bold" },
+              { text: "En tareas de traducción automática, los prompts se utilizan para solicitar la traducción de un texto de un idioma a otro. Por ejemplo, un prompt para traducción podría ser: \"Traduce el siguiente párrafo del inglés al español\".", content: "plain" },
+            ],
+          },
+          {
+            type: "li", 
+            info: [ 
+              { text: "Resumen de Texto:  ", content: "bold" },
+              { text: "Los prompts se utilizan para solicitar un resumen de un texto largo o complejo. Por ejemplo, un prompt para resumir un artículo científico podría ser: \"Resume los hallazgos principales de este estudio en no más de 100 palabras\".", content: "plain" },
+            ],
+          },
+          {
+            type: "li", 
+            info: [ 
+              { text: "Diálogo y Conversación:  ", content: "bold" },
+              { text: "En tareas de generación de diálogos o conversaciones, los prompts se utilizan para establecer el contexto y los participantes de la conversación. Por ejemplo, un prompt para generar un diálogo entre un cliente y un asistente virtual podría ser: \"Simula una conversación entre un cliente que necesita ayuda con su factura y un asistente virtual de servicio al cliente\".", content: "plain" },
+            ],
+          },
+          {
+            type: "li", 
+            info: [ 
+              { text: "Búsqueda de Respuestas:  ", content: "bold" },
+              { text: "Los prompts se utilizan para solicitar respuestas a preguntas específicas. Por ejemplo, un prompt para buscar información sobre un tema podría ser: \"¿Cuáles son las causas y consecuencias del cambio climático?\".", content: "plain" },
+            ],
+          },
+        ],
+      },
+      {
+        type: "p",
+        info: [
+          {
+            text: "En resumen, los prompts son herramientas fundamentales en la ingeniería de Prompt, ya que permiten dirigir y personalizar la generación de texto de modelos de lenguaje como GPT para una amplia gama de tareas específicas.",
+            content: "plain",
+          },
+        ],
+      },
+    ]
+  },
 ];
 
 export default classes;
