adae8d56da43952341844583a80a628ec7e46383
GuillermoCochrane <guilleac81@gmail.com>
2024-06-08 19:19:28 -0300

    Clase 8 de IA separada en temas

--- src/assets/data/IAChatGPT.js
+++ src/assets/data/IAChatGPT.js
@@ -1603,8 +1603,6 @@ let classes = [
           },
         ]
       },
-      
-      
     ]
   },
   {
@@ -1698,159 +1696,175 @@ let classes = [
   },
   { 
     class: 8,
+    topics: 2,
+    title: {
+      type: "h2",
+      info: [{ text: "Formatos de salida", content: "plain" }],
+    },
     classData: [
-      {
-        type: "h2",
-        info: [{ text: "Formatos de salida", content: "plain" }],
-      },
-      {
-        type: "h3",
-        info: [{ text: "Introducción a formatos de salida", content: "plain" }],
-      },
-      {
-        type: "p",
-        info: [
+      { 
+        topic: 1, 
+        topicData: [
           {
-            text: "Parte importante de un Prompt es el formato de salida que le solicitamos a ChatGPT ya que a partir de este se nos hará más fácil trabajar con los datos solicitados, a continuación, verás una lista con algunos ejemplos de formatos de salida que puede darte ChatGPT.",
-            content: "plain",
+            type: "h3",
+            info: [{ text: "Introducción a formatos de salida", content: "plain" }],
           },
-        ],
-      },
-      {
-        type: "ol",
-        info: [
           {
-            type: "li",
+            type: "p",
             info: [
               {
-                type: "h4",
+                text: "Parte importante de un Prompt es el formato de salida que le solicitamos a ChatGPT ya que a partir de este se nos hará más fácil trabajar con los datos solicitados, a continuación, verás una lista con algunos ejemplos de formatos de salida que puede darte ChatGPT.",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "ol",
+            info: [
+              {
+                type: "li",
                 info: [
                   {
-                    text: "Texto plano: ",
+                    type: "h4",
+                    info: [
+                      {
+                        text: "Texto plano: ",
+                        content: "plain",
+                      },
+                    ],
+                  },
+                  {
+                    text: "Ejemplo: ",
+                    content: "bold",
+                  },
+                  {
+                    text: "La fotosíntesis es el proceso mediante el cual las plantas y otros organismos convierten la luz solar en energía química para alimentarse y crecer.",
                     content: "plain",
                   },
                 ],
               },
               {
-                text: "Ejemplo: ",
-                content: "bold",
-              },
-              {
-                text: "La fotosíntesis es el proceso mediante el cual las plantas y otros organismos convierten la luz solar en energía química para alimentarse y crecer.",
-                content: "plain",
-              },
-            ],
-          },
-          {
-            type: "li",
-            info: [
-              {
-                type: "h4",
+                type: "li",
                 info: [
                   {
-                    text: "Texto plano: ",
+                    type: "h4",
+                    info: [
+                      {
+                        type: "h4", 
+                        info: [
+                          {
+                            text: "HTML: ", 
+                            content: "plain"
+                          }
+                        ]
+                      },
+                    ],
+                  },
+                  {
+                    text: "Ejemplo: ",
+                    content: "bold",
+                  },
+                  {
+                    text: "<p>El cielo nocturno está lleno de estrellas brillantes que forman constelaciones reconocibles, como la Osa Mayor y Orión.</p>",
                     content: "plain",
                   },
                 ],
               },
               {
-                text: "Ejemplo: ",
-                content: "bold",
+                type: "li",
+                info: [
+                  {type: "h4", info: [{text: "JSON: ", content: "plain"}]},
+                  {
+                    text: "Ejemplo: ",
+                    content: "bold",
+                  },
+                  {
+                    text: 'respuesta": "¿Sabías que los robots también tienen sueños? Bueno, al menos hasta que se quedan sin batería.',
+                    content: "plain",
+                  },
+                ],
               },
               {
-                text: "<p>El cielo nocturno está lleno de estrellas brillantes que forman constelaciones reconocibles, como la Osa Mayor y Orión.</p>",
-                content: "plain",
+                type: "li",
+                info: [
+                  {type: "h4", info: [{text: "Markdown: ", content: "plain"}]},
+                  {
+                    text: "Ejemplo: ",
+                    content: "bold",
+                  },
+                  {
+                    text: "La amistad es como una planta; necesita cuidado y atención para crecer y florecer.",
+                    content: "plain",
+                  },
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {type: "h4", info: [{text: "XML: ", content: "plain"}]},
+                  {
+                    text: "Ejemplo: ",
+                    content: "bold",
+                  },
+                  {
+                    text: "<respuesta>En un universo paralelo, los gatos son los gobernantes supremos y los humanos son sus leales siervos que les sirven atún fresco todos los días.</respuesta>",
+                    content: "plain",
+                  },
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {type: "h4", info: [{text: "CSV: ", content: "plain"}]},
+                  {
+                    text: "Ejemplo: ",
+                    content: "bold",
+                  },
+                  {
+                    text: 'respuesta: "Imagina un mundo donde todos los problemas se resuelven con abrazos en lugar de discusiones. Sería un lugar mucho más cálido y amoroso, ¿no crees?"',
+                    content: "plain",
+                  },
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Salida personalizada, cuanto más contexto mejor!",
+                    content: "plain",
+                  },
+                ],
               },
             ],
           },
-          {
-            type: "li",
-            info: [
-              {type: "h4", info: [{text: "HTML: ", content: "plain"}]},
-              {
-                text: "Ejemplo: ",
-                content: "bold",
-              },
-              {
-                text: 'respuesta": "¿Sabías que los robots también tienen sueños? Bueno, al menos hasta que se quedan sin batería.',
-                content: "plain",
-              },
-            ],
-          },
-          {
-            type: "li",
-            info: [
-              {type: "h4", info: [{text: "JSON: ", content: "plain"}]},
-              {
-                text: "Ejemplo: ",
-                content: "bold",
-              },
-              {
-                text: "La amistad es como una planta; necesita cuidado y atención para crecer y florecer.",
-                content: "plain",
-              },
-            ],
-          },
-          {
-            type: "li",
-            info: [
-              {type: "h4", info: [{text: "Markdown: ", content: "plain"}]},
-              {
-                text: "Ejemplo: ",
-                content: "bold",
-              },
-              {
-                text: "<respuesta>En un universo paralelo, los gatos son los gobernantes supremos y los humanos son sus leales siervos que les sirven atún fresco todos los días.</respuesta>",
-                content: "plain",
-              },
-            ],
-          },
-          {
-            type: "li",
-            info: [
-              {type: "h4", info: [{text: "CSV: ", content: "plain"}]},
-              {
-                text: "Ejemplo: ",
-                content: "bold",
-              },
-              {
-                text: 'respuesta: "Imagina un mundo donde todos los problemas se resuelven con abrazos en lugar de discusiones. Sería un lugar mucho más cálido y amoroso, ¿no crees?"',
-                content: "plain",
-              },
-            ],
-          },
-          {
-            type: "li",
-            info: [
-              {
-                text: "Salida personalizada, cuanto más contexto mejor!",
-                content: "plain",
-              },
-            ],
-          },
-        ],
+        ]
       },
       {
-        type: "h3",
-        info: [{ text: "Evaluación de Prompts", content: "plain" }],
-      },
-      {
-        type: "p",
-        info: [
+        topic: 2, 
+        topicData: [
           {
-            text: "Una vez optimizados los prompts, es crucial evaluar su efectividad en diferentes escenarios y tareas. Esto puede implicar la comparación de las respuestas generadas por el modelo utilizando diferentes variantes de prompts, así como la recopilación de métricas objetivas, como la precisión, coherencia y relevancia de las respuestas. Además, es importante considerar la retroalimentación cualitativa de los usuarios finales para evaluar la utilidad y adecuación de las respuestas generadas.",
-            content: "plain",
+            type: "h3",
+            info: [{ text: "Evaluación de Prompts", content: "plain" }],
           },
-        ],
-      },
-      {
-        type: "p",
-        info: [
           {
-            text: "Supongamos que estamos utilizando ChatGPT para generar respuestas a preguntas sobre salud. Después de optimizar el prompt utilizando técnicas mencionadas previamente, podemos evaluar su efectividad en términos de precisión y relevancia de las respuestas generadas. Podemos comparar las respuestas generadas por el modelo utilizando el prompt original y el prompt optimizado, y recopilar métricas objetivas, como la proporción de respuestas precisas y la coherencia del contenido. Además, podemos solicitar la retroalimentación de profesionales de la salud para evaluar la utilidad y precisión de las respuestas generadas en diferentes escenarios clínicos.",
-            content: "plain",
+            type: "p",
+            info: [
+              {
+                text: "Una vez optimizados los prompts, es crucial evaluar su efectividad en diferentes escenarios y tareas. Esto puede implicar la comparación de las respuestas generadas por el modelo utilizando diferentes variantes de prompts, así como la recopilación de métricas objetivas, como la precisión, coherencia y relevancia de las respuestas. Además, es importante considerar la retroalimentación cualitativa de los usuarios finales para evaluar la utilidad y adecuación de las respuestas generadas.",
+                content: "plain",
+              },
+            ],
           },
-        ],
+          {
+            type: "p",
+            info: [
+              {
+                text: "Supongamos que estamos utilizando ChatGPT para generar respuestas a preguntas sobre salud. Después de optimizar el prompt utilizando técnicas mencionadas previamente, podemos evaluar su efectividad en términos de precisión y relevancia de las respuestas generadas. Podemos comparar las respuestas generadas por el modelo utilizando el prompt original y el prompt optimizado, y recopilar métricas objetivas, como la proporción de respuestas precisas y la coherencia del contenido. Además, podemos solicitar la retroalimentación de profesionales de la salud para evaluar la utilidad y precisión de las respuestas generadas en diferentes escenarios clínicos.",
+                content: "plain",
+              },
+            ],
+          },
+        ]
       },
     ]
   },
