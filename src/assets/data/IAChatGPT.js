c7e5ed9e9cf2181a3c6925ad94b30519bc325ce7
GuillermoCochrane <guilleac81@gmail.com>
2024-06-08 18:57:42 -0300

    Clase 7 de IA separada en temas

--- src/assets/data/IAChatGPT.js
+++ src/assets/data/IAChatGPT.js
@@ -1609,86 +1609,88 @@ let classes = [
   },
   {
     class: 7,
+    topics: 1,
+    title: {
+      type: "h2",
+      info: [{ text: "Optimización y Ajuste de Prompts en Ingeniería de Prompt", content: "plain" }],
+    },
     classData: [
       {
-        type: "h2",
-        info: [{ text: "Optimización y Ajuste de Prompts en Ingeniería de Prompt", content: "plain" }],
-      },
-      {
-        type: "h3",
-        info: [{ text: "Introducción", content: "plain" }],
-      },
-      {
-        type: "p",
-        info: [
+        topic: 1,
+        topicData: [
           {
-            text: "En el contexto de la ingeniería de prompt, la optimización y ajuste de prompts son procesos fundamentales para mejorar la efectividad de los modelos de lenguaje, como ChatGPT. Estos procesos implican la revisión y modificación de los prompts para obtener mejores resultados en diferentes tareas y escenarios.",
-            content: "plain",
-          },
-        ],
-      },
-      {
-        type: "h3",
-        info: [{ text: "Pasos para la optimización de un Prompt", content: "plain" }],
-      },
-      {
-        type: "ul",
-        info: [
-          {
-            type: "li",
+            type: "p",
             info: [
-              { text: "Análisis de Resultados: ", content: "bold" },
               {
-                text: "Antes de optimizar un prompt, es crucial realizar un análisis exhaustivo de los resultados generados por el modelo en respuesta al prompt original. Esto implica revisar la calidad, relevancia y coherencia de las respuestas para identificar áreas de mejora. Además, es importante considerar la retroalimentación del usuario final para comprender mejor las necesidades y expectativas.",
+                text: "En el contexto de la ingeniería de prompt, la optimización y ajuste de prompts son procesos fundamentales para mejorar la efectividad de los modelos de lenguaje, como ChatGPT. Estos procesos implican la revisión y modificación de los prompts para obtener mejores resultados en diferentes tareas y escenarios.",
                 content: "plain",
               },
             ],
           },
           {
-            type: "li",
+            type: "h3",
+            info: [{ text: "Pasos para la optimización de un Prompt", content: "plain" }],
+          },
+          {
+            type: "ul",
             info: [
-              { text: "Refinamiento de Palabras Clave: ", content: "bold" },
               {
-                text: "Una técnica efectiva para mejorar un prompt es refinar las palabras clave utilizadas para guiar al modelo. Esto implica seleccionar términos más específicos y relevantes para la tarea en cuestión, lo que ayuda al modelo a generar respuestas más precisas y centradas. Por ejemplo, en lugar de usar \"inteligencia artificial\", se puede refinar a \"aplicaciones de inteligencia artificial en la atención médica\".",
-                content: "plain",
+                type: "li",
+                info: [
+                  { text: "Análisis de Resultados: ", content: "bold" },
+                  {
+                    text: "Antes de optimizar un prompt, es crucial realizar un análisis exhaustivo de los resultados generados por el modelo en respuesta al prompt original. Esto implica revisar la calidad, relevancia y coherencia de las respuestas para identificar áreas de mejora. Además, es importante considerar la retroalimentación del usuario final para comprender mejor las necesidades y expectativas.",
+                    content: "plain",
+                  },
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Refinamiento de Palabras Clave: ", content: "bold" },
+                  {
+                    text: "Una técnica efectiva para mejorar un prompt es refinar las palabras clave utilizadas para guiar al modelo. Esto implica seleccionar términos más específicos y relevantes para la tarea en cuestión, lo que ayuda al modelo a generar respuestas más precisas y centradas. Por ejemplo, en lugar de usar \"inteligencia artificial\", se puede refinar a \"aplicaciones de inteligencia artificial en la atención médica\".",
+                    content: "plain",
+                  },
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Incorporación de Ejemplos: ", content: "bold" },
+                  { text: "Incluir ejemplos concretos en el prompt puede ayudar al modelo a comprender mejor la tarea y generar respuestas más precisas. Estos ejemplos pueden proporcionar contextos específicos y guiar al modelo hacia el tipo de respuesta deseada. Por ejemplo, en un prompt para generar un resumen, se pueden incluir ejemplos de resúmenes bien estructurados para ilustrar el formato y contenido esperados.", content: "plain" },
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Experimentación Iterativa: ", content: "bold" },
+                  {
+                    text: "El proceso de optimización y ajuste de prompts a menudo requiere experimentación iterativa, donde se prueban diferentes variantes del prompt y se evalúan los resultados. Esto puede implicar modificar la estructura del prompt, ajustar las palabras clave o agregar restricciones adicionales. La experimentación iterativa permite identificar la combinación óptima de elementos del prompt que maximizan la calidad de las respuestas del modelo.",
+                    content: "plain",
+                  },
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Convierte tareas complejas en subtareas más simples:", content: "bold" },
+                  {
+                    text: "Para tareas en las que se requieren múltiples conjuntos de instrucciones independientes para manejar diferentes casos, puede ser beneficioso comenzar clasificando el tipo de consulta y utilizar esa clasificación para determinar qué instrucciones se necesitan. Por ejemplo, si vas a hacer el lanzamiento de un producto, es mejor pedirle al ChatGPT que te enumere las tareas y después trabajar sobre ellas.",
+                    content: "plain",
+                  },
+                ],
               },
             ],
           },
           {
-            type: "li",
+            type: "p",
             info: [
-              { text: "Incorporación de Ejemplos: ", content: "bold" },
-              { text: "Incluir ejemplos concretos en el prompt puede ayudar al modelo a comprender mejor la tarea y generar respuestas más precisas. Estos ejemplos pueden proporcionar contextos específicos y guiar al modelo hacia el tipo de respuesta deseada. Por ejemplo, en un prompt para generar un resumen, se pueden incluir ejemplos de resúmenes bien estructurados para ilustrar el formato y contenido esperados.", content: "plain" },
-            ],
-          },
-          {
-            type: "li",
-            info: [
-              { text: "Experimentación Iterativa: ", content: "bold" },
               {
-                text: "El proceso de optimización y ajuste de prompts a menudo requiere experimentación iterativa, donde se prueban diferentes variantes del prompt y se evalúan los resultados. Esto puede implicar modificar la estructura del prompt, ajustar las palabras clave o agregar restricciones adicionales. La experimentación iterativa permite identificar la combinación óptima de elementos del prompt que maximizan la calidad de las respuestas del modelo.",
+                text: "¡Un pequeño truco al terminar de optimizar tu prompt recomiendo pedirle a ChatGPT que te realice una plantilla autocompletable para llegar a la misma respuesta, sintetizando más aun el trabajo de usar el chat! Esto es muy útil cuando tenes que transformar gran cantidad de texto plano en un nuevo formato.",
                 content: "plain",
               },
             ],
-          },
-          {
-            type: "li",
-            info: [
-              { text: "Convierte tareas complejas en subtareas más simples:", content: "bold" },
-              {
-                text: "Para tareas en las que se requieren múltiples conjuntos de instrucciones independientes para manejar diferentes casos, puede ser beneficioso comenzar clasificando el tipo de consulta y utilizar esa clasificación para determinar qué instrucciones se necesitan. Por ejemplo, si vas a hacer el lanzamiento de un producto, es mejor pedirle al ChatGPT que te enumere las tareas y después trabajar sobre ellas.",
-                content: "plain",
-              },
-            ],
-          },
-        ],
-      },
-      {
-        type: "p",
-        info: [
-          {
-            text: "¡Un pequeño truco al terminar de optimizar tu prompt recomiendo pedirle a ChatGPT que te realice una plantilla autocompletable para llegar a la misma respuesta, sintetizando más aun el trabajo de usar el chat! Esto es muy útil cuando tenes que transformar gran cantidad de texto plano en un nuevo formato.",
-            content: "plain",
           },
         ],
       },
