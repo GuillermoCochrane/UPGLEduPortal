d67382a9cb9879511fa1bdb1bd8a0d2af7506d50
GuillermoCochrane <guilleac81@gmail.com>
2024-06-07 21:21:54 -0300

    Implementado nuevo error 404 utilizando comoponente previo

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -1,13 +1,7 @@
-import { Link } from "react-router-dom";
+import NotFound from "../partials/NotFound/NotFound";
 function Error404() {   
     return (
-        <article className="error-404">
-            <h2>Error 404</h2>
-            <p>
-                No se encontró la página que buscaba.
-            </p>
-            <Link to="/">Volver al inicio</Link>
-        </article>
+        <NotFound />
     );
 }
 
