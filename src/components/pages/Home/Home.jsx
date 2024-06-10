34328a7acd6a2203421e531179e7aa0e3cc2aa36
GuillermoCochrane <guilleac81@gmail.com>
2024-06-10 16:49:37 -0300

    Modificada Home

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,17 +1,9 @@
+import HomeData from '../../partials/HomeData/HomeData';
 function Home() {
   return (
-    <>
       <article>
-        <h2>Curso Inteligencia artificial y ChatGPT</h2>
-        <p>
-          🌟📚 El objetivo de esta página aprender un poco más sobre Inteligencia Argentina y, sobre todo, el buen uso de ChatGPT. En la misma no solo encontrarán la historia completa de la Inteligencia Artificial y ChatGPT, sino también cómo mejorar la redacción de los Prompts. 👌  📖 Nuevamente quería agradecerles la asistencia al curso y cualquier duda que tengan, no duden en consultarnos
-        </p>
-        <br/>
-        <p>
-          La idea principal del curso es onocer la historia de la inteligencia artificial, como funciona, como fue creada y aprender a comunicarte de manera correcta con ChatGPT.
-        </p>
+        <HomeData />
       </article>
-    </>
   )
 }
 
