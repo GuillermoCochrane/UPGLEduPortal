c6944eeefd9c74a664ea0e698fa6f41d2e6703ee
GuillermoCochrane <guilleac81@gmail.com>
2024-06-10 19:30:16 -0300

    Componente para renderizar los temas de cada clase

---
+++ src/components/pages/Topics/Topics.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react';
+import OL from '../../partials/OL/OLTag';
+import UL from '../../partials/UL/ULTag';
+import PTag from '../../partials/P/PTag';
+import H4 from '../../partials/H4/H4Tag';
+import H3 from '../../partials/H3/H3Tag';
+import H2 from '../../partials/H2/H2Tag';
+import Figure from '../../partials/Figure/Figure';
+import classes from  "../../../assets/data/IAChatGPT.js";
+import NavButtons from '../../partials/Navbuttons/NavButtons';
+import NotFound from '../../partials/NotFound/NotFound';
+
+function Topics(params) {
+  const classID  = parseInt(params.match.params.classId);
+  const topicID = parseInt(params.match.params.topicId);
+  const [classData, setClassData] = useState([]);
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [title, setTitle] = useState([]);
+  
+
+
+  useEffect(() => {
+    let newData = classes.filter(item => item.class === classID);
+    (newData.length === 0) ?
+      setClassData([]) :
+      setClassData(newData[0].classData);
+      setPage(classID);
+      newData.length > 0 ? setTitle(newData[0].title.info) : null;
+  }, [classID]);
+
+  useEffect(() => {
+    let newData = classData.filter(item => item.topic == topicID);
+    (newData.length === 0) ?
+      setData([]) :
+      setData(newData[0].topicData);
+  }, [classData, topicID]);
+
+  
+
+  return (
+    <article>
+      <H2  Data={title} />
+      {
+        data.length == 0 ? <NotFound /> :
+        data.map((item, index) => (
+          item.type === "h2" ? <H2 key={index} Data={item.info} /> :
+          item.type === "figure" ? <Figure key={index} Data={item.info} /> :
+          item.type === "h3" ? <H3 key={index}  Data={item.info} /> :
+          item.type === "h4" ? <H4 key={index} Data={item.info} /> :
+          item.type === "p" ? <PTag key={index} Data={item.info} /> :
+          item.type === "ul" ? <UL key={index} Data={item.info} /> :
+          item.type === "ol" ? <OL key={index} Data={item.info} /> : null
+        ))
+      }
+      {
+        data.length == 0 ? null : 
+        <NavButtons Page={page} LastPage={data.length} />
+      }
+      
+    </article>
+  );
+}
+
+export default Topics;
