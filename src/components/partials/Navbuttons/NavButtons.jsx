2aa1ae09e0813c258680f835a3a71ebf826b19b9
GuillermoCochrane <guilleac81@gmail.com>
2024-06-11 01:51:38 -0300

    Modificados botones de navegación para adaptarlos a la nueva estructura del sitio

--- src/components/partials/Navbuttons/NavButtons.jsx
+++ src/components/partials/Navbuttons/NavButtons.jsx
@@ -1,31 +1,37 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-function NavButtons({ Page, LastPage }) {
-    console.log(LastPage);
+function NavButtons({ Page }) {
     return (
         <footer>
             
             {
-                Page == 1 ? 
-                null :
-                <Link to={`/class/${Page-1}`}>
-                    <button >Anterior</button>
-                </Link>
+                Page.topic == 1 && Page.class == 1 ?
+                    null :
+                    Page.topic == 1 ?
+                    <Link to={`/class/${Page.class-1}/${Page.lastClassLastTopic}`}>
+                        <button >Clase Anterior</button>
+                    </Link> :
+                        <Link to={`/class/${Page.class}/${Page.topic-1}`}>
+                            <button >Anterior</button>
+                        </Link>
             }    
             {
-                Page == LastPage ? 
+                Page.topic == Page.topics && Page.class == Page.classes ?
                 null :
-                <Link to={`/class/${Page+1}`}>
-                    <button >Siguiente</button>
-                </Link>
+                    Page.topic == Page.topics ?
+                    <Link to={`/class/${Page.class+1}/1`}>
+                        <button >Siguiente Clase</button>
+                    </Link> :
+                        <Link to={`/class/${Page.class}/${Page.topic+1}`}>
+                            <button >Siguiente</button>
+                        </Link>
             }
         </footer>
     );
 }
 
 NavButtons.propTypes = {
-    Page: PropTypes.number.isRequired,
-    LastPage: PropTypes.number.isRequired
+    Page: PropTypes.object.isRequired,
 };
 
 export default NavButtons;
