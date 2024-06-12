23eaa005d58d49ada455ade4581d2ca489e9ae16
GuillermoCochrane <guilleac81@gmail.com>
2024-06-12 12:30:00 -0300

    Nuevo Home con índice de cursos

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,9 +1,28 @@
-import HomeData from '../../partials/HomeData/HomeData';
+import {Link} from "react-router-dom";
+import coursesIndex from '../../../assets/data/CoursesIndex'
+import Logo from "../../../assets/images/icon.png";
+
 function Home() {
+
   return (
-      <article>
-        <HomeData />
-      </article>
+    <article className="home">
+      <figure >
+        <img src={Logo} alt="Logo de UPGL" className="logo" />
+      </figure>
+        <h1 className="UPGL" >Universidad Popular General Levalle</h1>
+        <h2>Seleccione su curso</h2>
+        <section>
+        {
+          coursesIndex.map((item, index) => (
+            <h3 key={index}>
+              <Link to={item.links.link} key={index}>
+                {item.links.title}
+              </Link>
+            </h3>
+          ))
+        }
+        </section>
+    </article>
   )
 }
 
