078901a09a729f69dd3d320df576cde34c7ea0d4
GuillermoCochrane <guilleac81@gmail.com>
2024-06-12 12:29:08 -0300

    Modificado componente app.jsx para adaptar la nueva estructura de Rutas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,17 @@
-import { useRef } from 'react'
-import { Route , Switch, Link } from "react-router-dom";
-//import Home from "./pages/Home/Home"
-//import Class from "./pages/Class/Class"
-//import NavIndex from "./partials/NavBarIndex/NavIndex"
-import index from "../assets/data/IAChatGPTIndex"
+import { Route , Switch } from "react-router-dom";
 import Error404 from "./pages/Error404/Error404"
-import ClassIndex from "./pages/ClassIndex/ClassIndex"
-import Topics from './pages/Topics/Topics';
-import NavBarIndex from "./partials/NavBarIndex/NabBarIndex"
+import Courses from "./pages/Courses/Courses";
+import Home from "./pages/Home/Home";
 
 function App() {
 
-  const navBar = useRef(null);
-
-  const handleClick = () => {
-    navBar.current.classList.toggle("hidden");
-  }
-
   return (
-    <>
-    <header>
-        <button onClick={handleClick} id="index">Índice</button>
-        <h1>Curso de ChatGPT e Inteligencia artificial</h1>
-    </header>
-    <main>
-      <nav className='sumario' ref={navBar}>
-        <ol>
-          {
-            index.map((item, index) => (
-              <NavBarIndex data={item} key={index} />
-            ))
-          }
-        </ol>
-        <Link to="/">
-          <button >Volver al inicio</button>
-        </Link> 
-      </nav>
       <Switch>
-        <Route path="/" exact component={ClassIndex} />
-        <Route path="/class" exact component={ClassIndex} />
-        <Route path="/class/:classId" exact component={ClassIndex} />
-        <Route path="/class/:classId/:topicId" exact component={Topics} />
+        <Route path="/" exact component={Home} />
+        <Route path="/courses" exact component={Home} />
+        <Route path="/courses/:courseId" exact component={Courses} />
         <Route component={Error404} />
       </Switch>
-    </main>
-    
-    </>
   )
 }
 
