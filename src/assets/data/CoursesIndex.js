58a0cabe3dea779fe88512b73fcf161426e921ce
GuillermoCochrane <guilleac81@gmail.com>
2024-06-12 12:26:05 -0300

    Creada DB de cursos y sus links

---
+++ src/assets/data/CoursesIndex.js
@@ -0,0 +1,21 @@
+const CoursesIndex = [
+    {
+        courseId : 1,
+        links : 
+            {
+                title : "Introducción a la IA y chat GPT",
+                link : "/courses/IA"
+            }
+        
+    },
+    {
+        courseId : 2,
+        links :    
+            {
+                title: "Curso de Python",
+                link : "/courses/Python"
+            }
+    }
+]
+
+export default CoursesIndex;
