64a178dff510b1632b2624af4cf1a831f63d90e0
GuillermoCochrane <guilleac81@gmail.com>
2024-06-12 22:33:35 -0300

    Agregada DB de índice de clases de curso de Python

---
+++ src/assets/data/PythonIndex.js
@@ -0,0 +1,26 @@
+let classIndex = [
+    {
+      summary: "Introduccion a la programación",
+      classId: 1,
+      links: [
+        {
+          title: "¿Qué es la programación?",
+          link: "/courses/Python/class/1/1"
+        },
+        {
+          title: "¿QUE ES UN PROGRAMADOR?",
+          link: "/courses/Python/class/1/2"
+        },
+        {
+          title: "¿QUE ES UN ALGORITMO?",
+          link: "/courses/Python/class/1/3"
+        },
+        {
+          title: "¿Qué es la lógica?",
+          link: "/courses/Python/class/1/4"
+        }
+      ]
+    }
+  ]
+
+export default  classIndex
