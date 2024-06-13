1343444fa4649c64b29c8c04de934a56d0e11570
GuillermoCochrane <guilleac81@gmail.com>
2024-06-13 16:45:25 -0300

    Modificado Componente para que mantega abierta el índice de la clase en que me encuentro

--- src/components/partials/NavBarIndex/NabBarIndex.jsx
+++ src/components/partials/NavBarIndex/NabBarIndex.jsx
@@ -1,9 +1,19 @@
 import NavIndex from './NavIndex';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 function NabBarIndex({data}) {
+  const [classID, setClassID] = useState(0);  
+  let urlArray = location.pathname.split("/");
+  let classIDParams = parseInt(urlArray[4]);
+
+  useEffect(() => {
+    classIDParams ? setClassID(classIDParams) : null;
+  }, [classIDParams]);
+
+
   return (
           <li>
-            <details >
+            <details open={data.classId === classID}>
               <summary>{data.summary}</summary>
               <ol>
                 {
