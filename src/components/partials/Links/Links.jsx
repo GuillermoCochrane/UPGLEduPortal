ad1cb2461f4e1aa53acade9ca9e4bda5326c0c8b
GuillermoCochrane <guilleac81@gmail.com>
2024-06-13 18:51:28 -0300

    Componente para hipervinculos

---
+++ src/components/partials/Links/Links.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import Bold from '../Bold/Bold'
+function Links({Data}) {
+    return (
+            <>
+            {
+                Data.map((item, index) => (
+                    <a href={item.link} target="_blank" rel="noopener noreferrer" role='button' key={index}>
+                        {
+                            item.content == "plain" && item.text
+                        }
+                        {
+                            item.content == "bold" && <Bold key = {index} Data = {item.text} />
+                        }
+                    </a>
+                ))
+            }
+            </>
+        
+    );
+}
+
+Links.propTypes = {
+    Data: PropTypes.array.isRequired
+};
+
+export default Links;
