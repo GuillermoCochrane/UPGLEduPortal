58ad390202480dec8ebcd9777cc8c1188f472eab
GuillermoCochrane <guilleac81@gmail.com>
2024-06-21 01:07:49 -0300

    Agregado parte del tema 8 de la clase 6

--- src/assets/data/Python.js
+++ src/assets/data/Python.js
@@ -5745,6 +5745,38 @@ let classes = [
           }
         ]
       },
+      {
+        topic: 8,
+        topicData: [
+          {
+            type: "h3",
+            info: [
+              {
+                text: "Bonus Track:",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "h4",
+            info: [
+              {
+                text: '¿Por qué "Python" se llama así?',
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: 'Guido van Rossum, el creador, quería un nombre corto, único y un poco misterioso. En ese momento, él había estado leyendo los Guiones publicados de Flying Circus de Monty Python, un popular programa de la BBC, era un programa de comedia de los años 1970. (Supuestamente obtuvimos el término "spam" de una parodia en ese programa.) Le gustó tanto el programa que decidió nombrar su nuevo lenguaje de programación en homenaje al mismo. Esto demuestra que los programadores tienen sentido del humor.',
+                content: "plain",
+              },
+            ],
+          },
+        ]
+      }
     ]
   }
 ];
