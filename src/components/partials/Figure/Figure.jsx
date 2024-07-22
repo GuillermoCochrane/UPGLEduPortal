9767a070b4382597874963a52dbb8528ceb9b9c2
GuillermoCochrane <guilleac81@gmail.com>
2024-07-22 18:58:53 -0300

    Funcionalidad de zoom en vista mobile

--- src/components/partials/Figure/Figure.jsx
+++ src/components/partials/Figure/Figure.jsx
@@ -1,20 +1,33 @@
 import PropTypes from "prop-types";
+import { useRef } from "react";
+
 function Figure({Data}) {
-  return (
-    <figure>
-        <img 
-          src={Data.img} 
-          alt={Data.alt}
-          className={ 
-            Data.style == "icon" ? "icon" : 
-            Data.style == "info" ? "info" : 
-            ""} />
-    </figure>
-  );
+	const $img = useRef(null);
+
+	const zoom = () => {
+		if (window.innerWidth < 768) {
+			$img.current.classList.toggle('enlarged');
+	}}
+
+	return (
+		<figure>
+			<img 
+				ref={$img}
+				onClick={zoom}
+				src={Data.img} 
+				alt={Data.alt}
+				title={Data.alt}
+				className={ 
+					Data.style == "icon" ? "icon" : 
+					Data.style == "info" ? "info" : 
+					""} 
+			/>
+		</figure>
+	);
 }
 
 Figure.propTypes = {
-  Data: PropTypes.object.isRequired
+	Data: PropTypes.object.isRequired
 };
 
 export default Figure;
