1a6ec010c8a2b1d6337bdf5f967d0c1aa132a5e1
GuillermoCochrane <guilleac81@gmail.com>
2024-09-21 10:27:35 -0300

    Agregado tema 2 a la clase 13

--- src/assets/data/Python.js
+++ src/assets/data/Python.js
@@ -12770,6 +12770,106 @@ let classes = [
           },
         ]
       },
+      {
+        topic: 2,
+        topicData: [
+          {
+            type: "h3",
+            info: [
+              {
+                text: "Creación y Uso de Objetos",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "h4",
+            info: [
+              {
+                text: "Creando Instancias",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "Para crear un objeto a partir de una clase, se utiliza la palabra clave ",
+                content: "plain",
+              },
+              {
+                text: "__init__",
+                content: "bold",
+              },
+              {
+                text: " (constructor) que se llama automáticamente cuando se crea un objeto. En Python, no se utiliza ",
+                content: "plain",
+              },
+              {
+                text: "new",
+                content: "bold",
+              },
+              {
+                text: ", simplemente se llama a la clase. Cada objeto creado es una instancia única de la clase.",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "h4",
+            info: [
+              {
+                text: "Accediendo a Atributos",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "Se acceden a los atributos de un objeto utilizando la notación de punto. Por ejemplo, ",
+                content: "plain",
+              },
+              {
+                text: "objeto.atributo",
+                content: "bold",
+              },
+              {
+                text: ".",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "h4",
+            info: [
+              {
+                text: "Invocando Métodos",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "Se invocan métodos de un objeto utilizando la notación de punto seguida del nombre del método y argumentos entre paréntesis. Por ejemplo, ",
+                content: "plain",
+              },
+              {
+                text: "objeto.metodo(argumento1, argumento2)",
+                content: "bold",
+              },
+              {
+                text: ".",
+                content: "plain",
+              },
+            ],
+          },
+        ]
+      },
     ]
   },
 ];
