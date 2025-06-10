c2cd14f8eee33d851ec7daac21d49bbbab1e22c2
GuillermoCochrane <guilleac81@gmail.com>
2025-06-10 10:18:49 -0300

    Agregado tema 2 de la clase 1 del curso de frontend

--- src/assets/data/FrontEnd.js
+++ src/assets/data/FrontEnd.js
@@ -38,7 +38,7 @@ const classes = [
             }
           },
           {
-            type: "h3",
+            type: "h4",
             info: [
               {
                 text: "¿Qué se puede hacer con la programación?",
@@ -114,7 +114,7 @@ const classes = [
             ],
           },
           {
-            type: "h3",
+            type: "h4",
             info: [ 
               {
                 text: "¿Qué se necesita para programar?",
@@ -167,7 +167,7 @@ const classes = [
             ],
           },
           {
-            type: "h3",
+            type: "h4",
             info: [
               {
                 text: "¿Es difícil aprender a programar?",
@@ -220,7 +220,7 @@ const classes = [
             ],
           },
           {
-            type: "h3",
+            type: "h4",
             info: [
               {
                 text: "¿Por qué es importante aprender a programar?",
@@ -260,6 +260,364 @@ const classes = [
             ],
           },
         ],
+      },
+      {
+        topic: 2,
+        topicData: [
+          {
+            type: "h3",
+            info: [
+              { text: "¿Qué es un programador?", content: "plain" },
+            ],
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "Un programador es un profesional que se dedica a crear y optimizar software informático. Para ello, utilizan lenguajes de programación que son similares a idiomas que permiten comunicarse con la computadora y escribir instrucciones que generan nuevos programas.",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "h4",
+            info: [
+              {
+                text: "Tipos de programadores",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "Por especialidad",
+                content: "bold",
+              },
+            ],
+          },
+          {
+            type: "ul",
+            info: [
+              {
+                type: "li",
+                info: [
+                  { 
+                    text: "Desarrollador web: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Crea y mantiene sitios web y aplicaciones web",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: " Desarrollador móvil: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Crea aplicaciones para dispositivos móviles",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Desarrollador de software: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Crea software para diferentes plataformas",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Desarrollador de videojuegos: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Crea videojuegos para diferentes plataformas",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Ingeniero de datos: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Trabaja con grandes conjuntos de datos",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Científico de datos: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Analiza datos y desarrolla modelos predictivos",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Ingeniero de IA: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Desarrolla sistemas de aprendizaje autónomo",
+                    content: "plain",
+                  }
+                ],
+              },
+            ],
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "Por lenguaje de programación",
+                content: "bold",
+              },
+            ],
+          },
+          {
+            type: "ul",
+            info: [
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Desarrollador Java: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Especializado en aplicaciones empresariales",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Desarrollador Python: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Análisis de datos, IA, web y desarrollo general",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Desarrollador C++: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Sistemas de bajo nivel, juegos y alto rendimiento",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Desarrollador JavaScript: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Aplicaciones web, móviles y de servidor",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Desarrollador PHP: ",
+                    content: "bold" 
+                  },
+                  {
+                    text: "Aplicaciones web y gestión de contenidos",
+                    content: "plain",
+                  }
+                ],
+              },
+            ],
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "Por experiencia",
+                content: "bold",
+              },
+            ],
+          },
+          {
+            type: "ul",
+            info: [
+              {
+                type: "li",
+                info: [
+                  { 
+                    text: "Junior: ", 
+                    content: "bold" 
+                  },
+                  {
+                    text: "Poca experiencia, generalmente recién graduado",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  { 
+                    text: "Semi Senior: ", 
+                    content: "bold" 
+                  },
+                  {
+                    text: "Conocimientos medios del rubro y programación",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  { 
+                    text: "Senior: ", 
+                    content: "bold" 
+                  },
+                  {
+                    text: "Mucha experiencia y conocimiento en su especialización",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  { 
+                    text: "Lead: ", 
+                    content: "bold" 
+                  },
+                  {
+                    text: "Lidera equipos y toma decisiones técnicas",
+                    content: "plain",
+                  }
+                ],
+              },
+            ],
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "Por rol",
+                content: "bold",
+              },
+            ],
+          },
+          {
+            type: "ul",
+            info: [
+              {
+                type: "li",
+                info: [
+                  { 
+                    text: "Desarrollador: ", 
+                    content: "bold" 
+                  },
+                  {
+                    text: "Se encarga de escribir el código",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  { 
+                    text: "QA: ", 
+                    content: "bold" 
+                  },
+                  {
+                    text: "Se encarga de probar el software",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  { 
+                    text: "DevOps: ", 
+                    content: "bold" 
+                  },
+                  {
+                    text: "Automatiza implementación y mantenimiento",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  { 
+                    text: "Gerente de proyecto: ", 
+                    content: "bold" 
+                  },
+                  {
+                    text: "Planifica y gestiona el desarrollo",
+                    content: "plain",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  { 
+                    text: "Arquitecto de software: ", 
+                    content: "bold" 
+                  },
+                  {
+                    text: "Diseña la estructura del sistema",
+                    content: "plain",
+                  }
+                ],
+              },
+            ],
+          },
+        ]
       },
     ],
   }
