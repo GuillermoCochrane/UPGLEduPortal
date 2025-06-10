556f85d379cfadd377adc29782422bcd0aa726d3
GuillermoCochrane <guilleac81@gmail.com>
2025-06-10 10:53:39 -0300

    Agregado tema 3 de la clase 1 del curso de frontend

--- src/assets/data/FrontEnd.js
+++ src/assets/data/FrontEnd.js
@@ -619,6 +619,217 @@ const classes = [
           },
         ]
       },
+      {
+        topic: 3,
+        topicData: [
+          {
+            type: "h3",
+            info: [
+              { text: "Historia y Evolución de la Web", content: "plain" },
+            ]
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "La web ha experimentado una notable evolución desde sus inicios.",
+                content: "plain",
+              }
+            ]
+          },
+          {
+            type: "h4",
+            info: [
+              {
+                text: "Evolución de la Web",
+                content: "plain",
+              }
+            ]
+          },
+          {
+            type: "link",
+            info: [
+              {
+                text: "Primera Web: CERN",
+                content: "plain",
+                link: "http://info.cern.ch/hypertext/WWW/TheProject.html",
+              }
+            ]
+          },
+          {
+            type: "h4",
+            info: [
+              {
+                text: "Web 1.0",
+                content: "plain",
+              }
+            ]
+          },
+          {
+            type: "ul",
+            info: [
+              {
+                type: "li",
+                info: [
+                  { text: "Fue la primera versión de Internet, desarrollada en los años 90", content: "plain" },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Conocida como \"World Wide Web\"", content: "plain" },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Características principales:", content: "plain" },
+                  {
+                    type: "ul",
+                    info: [
+                      {
+                        type: "li",
+                        info: [
+                          { text: "Era estática y de solo lectura", content: "plain" },
+                        ]
+                      },
+                      {
+                        type: "li",
+                        info: [
+                          { text: "Contenidos unidireccionales y estáticos", content: "plain" },
+                        ]
+                      },
+                      {
+                        type: "li",
+                        info: [
+                          { text: "Los sitios almacenaban contenidos directamente en archivos", content: "plain" },
+                        ],
+                      },
+                      {
+                        type: "li",
+                        info: [
+                          { text: "Formularios vinculados a correo electrónico", content: "plain" },
+                        ],
+                      },
+                    ]
+                  }
+                ]
+              }
+            ],
+          },
+          {
+            type: "h4",
+            info: [
+              {
+                text: "Web 2.0",
+                content: "plain",
+              }
+            ]
+          },
+          {
+            type: "ul",
+            info: [
+              {
+                type: "li",
+                info: [
+                  { text: "Permitió la interacción y colaboración entre usuarios", content: "plain" },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Incluyó redes sociales, blogs y wikis", content: "plain" },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Los usuarios se convirtieron en \"prosumidores\"", content: "plain" },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Fomenta la creación y compartición colaborativa de contenido", content: "plain" },
+                ]
+              }
+            ],
+          },
+          {
+            type: "h4",
+            info: [
+              {
+                text: "Web 3.0",
+                content: "plain",
+              }
+            ]
+          },
+          {
+            type: "ul",
+            info: [
+              {
+                type: "li",
+                info: [
+                  { text: "Se centra en la inteligencia artificial", content: "plain" },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Implementa web semántica", content: "plain" },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Evoluciona hacia espacios 3D", content: "plain" },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Los usuarios son dueños de sus datos", content: "plain" },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Control sobre cómo compartir la información", content: "plain" },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Utiliza tecnologías como:", content: "plain" },
+                  {
+                    type: "ul",
+                    info: [
+                      {
+                        type: "li",
+                        info: [
+                          { text: "Blockchain", content: "plain" },
+                        ]
+                      },
+                      {
+                        type: "li",
+                        info: [
+                          { text: "Criptomonedas", content: "plain" },
+                        ]
+                      },
+                      {
+                        type: "li",
+                        info: [
+                          { text: "Metaversos", content: "plain" },
+                        ]
+                      },
+                    ]
+                  }
+                ]
+              }
+            ],
+          },
+        ]
+      }
     ],
   }
 ];
