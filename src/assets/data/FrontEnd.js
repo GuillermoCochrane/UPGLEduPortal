673aa579e809360dc26ef222222136bef29da5bd
GuillermoCochrane <guilleac81@gmail.com>
2025-06-10 11:50:31 -0300

    Agregado tema 5 de la clase 1 del curso de frontend

--- src/assets/data/FrontEnd.js
+++ src/assets/data/FrontEnd.js
@@ -1084,6 +1084,99 @@ const classes = [
             ]
           }
         ]
+      },
+      {
+        topic: 5,
+        topicData: [
+          {
+            type: "h3",
+            info: [
+              { text: "Entorno de Trabajo", content: "plain" },
+            ]
+          },
+          {
+            type: "h4",
+            info: [
+              {
+                text: "Herramientas Básicas: ",
+                content: "bold",
+              },
+              {
+                text: "Editores de Código, Navegadores y el Inspector de Elementos",
+                content: "plain",
+              }
+            ]
+          },
+          {
+            type: "ul",
+            info: [
+              {
+                type: "li",
+                info: [
+                  { text: "Editor de código: ", content: "bold" },
+                  {
+                    text: "Visual Studio Code: ",
+                    content: "bold",
+                  },
+                  {
+                    text: "versátil y con extensiones que facilitan la programación. ",
+                    content: "plain",
+                  },
+                  {
+                    type: "link",
+                    info: [
+                      {
+                        text: "Descargar VS Code",
+                        content: "plain",
+                        link: "https://code.visualstudio.com/",
+                      }
+                    ]
+                  }
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { 
+                    text: "Navegadores Web: ", 
+                    content: "bold" 
+                  },
+                  {
+                    text: "Chrome, Firefox, Safari, Edge, Opera. Recomiendo ",
+                    content: "plain",
+                  },
+                  {
+                    type: "link",
+                    info: [
+                      {
+                        text: "¿Puedo Usarlo?",
+                        content: "plain",
+                        link: "https://caniuse.com/",
+                      }
+                    ]
+                  },
+                  {
+                    text: "Página para consultar compatibilidad.",
+                    content: "plain",
+                  }
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Inspector de Elementos: ",
+                    content: "bold",
+                  },
+                  {
+                    text: "Herramienta integrada (tecla F12) para depurar HTML/CSS y experimentar en tiempo real. ",
+                    content: "plain",
+                  },
+                ]
+              }
+            ]
+          }
+        ]
       }
     ],
   }
