25e022197d4063265d4dd3e1c0aef02b875cc70d
GuillermoCochrane <guilleac81@gmail.com>
2025-06-11 17:24:28 -0300

    Agregado tema 6 de la clase 1 del curso de frontend

--- src/assets/data/FrontEnd.js
+++ src/assets/data/FrontEnd.js
@@ -1179,6 +1179,585 @@ const classes = [
             ]
           }
         ]
+      },
+      {
+        topic: 6,
+        topicData: [
+          {
+            type: "h3",
+            info: [
+              { text: "¿Qué es HTML?", content: "plain" },
+            ]
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "HTML ",
+                content: "bold",
+              },
+              {
+                text: "(HyperText Markup Language) es el lenguaje estándar para crear y estructurar páginas web a través de etiquetas y atributos.",
+                content: "plain",
+              },
+            ]
+          },
+          {
+            type: "h3",
+            info: [
+              {
+                text: "Principales etiquetas HTML",
+                content: "plain",
+              }
+            ]
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "Las etiquetas HTML son los bloques fundamentales para construir páginas web. Cada tipo cumple una función específica en la estructura de tu documento.",
+                content: "plain",
+              }
+            ]
+          },
+          {
+            type: "ul",
+            info: [
+              {
+                type: "li",
+                info: [
+                  { text: "Encabezados: ", content: "bold" },
+                  {
+                    text: "Define la jerarquía y organización del contenido en la página.\n",
+                    content: "plain",
+                  },
+                  {
+                    type: "ul",
+                    info: [
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<h1>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Encabezado principal",
+                            content: "plain",
+                          }
+                        ] 
+                      },
+                      { 
+                        type: "li", 
+                        info: [
+                          { 
+                            text: "<h2>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Segundo nivel",
+                            content: "plain",
+                          }
+                        ] 
+                      },
+                      { 
+                        type: "li", 
+                        info: [
+                          {
+                            text: "<h3>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Tercer nivel",
+                            content: "plain",
+                          }
+                        ] 
+                      },
+                      {
+                        type: "li",
+                        info: [
+                          { 
+                            text: "… hasta ",
+                            content: "plain",
+                          },
+                          {
+                            text: "<h6>",
+                            content: "bold",
+                          }
+                        ] 
+                      },
+                    ]
+                  },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Texto y contenido: ", content: "bold" },
+                  {
+                    text: "Elementos fundamentales para mostrar y organizar tu contenido.",
+                    content: "plain",
+                  },
+                  {
+                    type: "ul",
+                    info: [
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<p>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Párrafo",
+                            content: "plain",
+                          }
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<a href=\"#\">",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Enlace",
+                            content: "plain"
+                          }
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          {
+                            text: "<img src=\"…\" alt=\"…\">",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Imagen",
+                            content: "plain"
+                          }
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<strong>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Texto en negrita",
+                            content: "plain"  
+                          }
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          {
+                            text: "<em>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Texto con cursiva",
+                            content: "plain"
+                          }
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<br>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Salto de línea",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { "text": "<hr>", content: "bold" },
+                          {
+                            text: " – Línea horizontal",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<span>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Contenedor en línea",
+                            content: "plain"
+                          }
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<div>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Contenedor en bloque",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                    ]
+                  },
+                  ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Listas: ", content: "bold" },
+                  {
+                    text: "Organiza información en formato de listas numeradas o con viñetas.",
+                    content: "plain",
+                  },
+                  {
+                    type: "ul",
+                    info: [
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<ul>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Lista desordenada",
+                            content: "plain"
+                          }
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<ol>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Lista ordenada",
+                            content: "plain"
+                          }
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          {
+                            text: "<li>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Elemento de lista",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                    ]
+                  },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Tablas: ", content: "bold" },
+                  {
+                    text: "Muestra datos tabulares organizados en filas y columnas.",
+                    content: "plain",
+                  },
+                  {
+                    type: "ul",
+                    info: [
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<table>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Tabla",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<tr>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Fila de tabla",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<td>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Celda",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<th>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Encabezado de celda",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                    ]
+                  },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Formularios e Interacción: ", content: "bold" },
+                  {
+                    text: "Permite a los usuarios interactuar y enviar información a través de la web.",
+                    content: "plain",
+                  },
+                  {
+                    type: "ul",
+                    info: [
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<form>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Formulario",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<input type=\"...\">",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Campos de entrada",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<button>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Botón",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<select> y <option>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Menú desplegable",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<textarea>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Área de texto",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                    ]
+                  },
+                  
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Multimedia: ", content: "bold" },
+                  {
+                    text: "Incorpora contenido multimedia como audio, video y elementos externos.",
+                    content: "plain",
+                  },
+                  {
+                    type: "ul",
+                    info: [
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<audio>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Audio",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<video>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Video",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<source>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Fuente multimedia",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<iframe>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Contenido externo",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                    ]
+                  },
+                ]
+              },
+              {
+                type: "li",
+                info: [
+                  { text: "Otros elementos importantes: ", content: "bold" },
+                  {
+                    text: "Elementos para incorporar scripts, estilos y comportamientos a tu página.",
+                    content: "plain",
+                  },
+                  {
+                    type: "ul",
+                    info: [
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<script>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – JavaScript",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<link>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Hojas de estilo o iconos",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<style>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – CSS interno",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                      { 
+                        type: "li",
+                        info: [
+                          { 
+                            text: "<noscript>",
+                            content: "bold",
+                          },
+                          {
+                            text: " – Contenido sin JS",
+                            content: "plain"
+                          },
+                        ] 
+                      },
+                    ]
+                  },
+                ]
+              }
+            ]
+          },
+        ]
       }
     ],
   }
