ea9f339ca88180b8201096c78d009e97b027c8d0
GuillermoCochrane <guilleac81@gmail.com>
2025-06-21 21:47:57 -0300

    Agregada clase 6 del tema 4

--- src/assets/data/FrontEnd.js
+++ src/assets/data/FrontEnd.js
@@ -4383,7 +4383,59 @@ const classes = [
             }
           }
         ]
+      },
+      {
+        topic: 6,
+        topicData: [
+          {
+            type: "h3",
+            info: [
+              { text: "Inputs Especiales", content: "plain" }
+            ]
+          },
+          {
+            type: "p",
+            info: [
+              { text: "Los inputs especiales ofrecen funcionalidades poco comunes o específicas, como campos ocultos o botones de imagen.", content: "plain" }
+            ]
+          },
+
+          { type: "h4", info: [{ text: "Campo Oculto (Hidden):", content: "plain" }] },
+
+          {
+            type: "inputCard",
+            info: {
+              label: [
+                { text: "Hidden:", content: "bold" },
+                { text: " Campo oculto (no visible)", content: "plain" }
+              ],
+              type: "hidden",
+              attributes: { value: "valor_oculto" },
+              code: { content: "<input type=\"hidden\" value=\"valor_oculto\">" }
+            }
+          },
+
+          { type: "h4", info: [{ text: "Botón de Imagen (Image):", content: "plain" }] },
+
+          {
+            type: "inputCard",
+            info: {
+              label: [
+                { text: "Image:", content: "bold" },
+                { text: " Botón de imagen", content: "plain" }
+              ],
+              type: "image",
+              attributes: { 
+                src: "https://i.imgur.com/kKZ8t1j.png", 
+                alt: "Enviar",
+                width: "150"
+              },
+              code: { content: "<input type=\"image\" src=\"imagen.png\" alt=\"Enviar\">" }
+            }
+          }
+        ]
       }
+
     ]
   },
 ]
