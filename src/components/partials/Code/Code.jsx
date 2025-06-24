1bedbb73b59466e60291decd2195c52d437cdc64
GuillermoCochrane <guilleac81@gmail.com>
2025-06-24 18:44:43 -0300

    Componente modificado para implemetar prism.js

--- src/components/partials/Code/Code.jsx
+++ src/components/partials/Code/Code.jsx
@@ -1,11 +1,22 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+import 'prismjs';
+import 'prismjs/themes/prism-okaidia.css'; // o el tema que elijas
+import 'prismjs/components/prism-http.min'; 
 import './code.css';
 
 function Code({ Data }) {
+  
+  useEffect(() => {
+    import('prismjs').then(Prism => {
+      Prism.highlightAll();
+    });
+  }, [Data]);
+
   return (
     <div className="code-container">
       <pre>
-        <code className={`language-${ 'html'}`}>
+        <code className={`language-${Data.language || 'html'}`}>
           {Data.content}
         </code>
       </pre>
