68c999a01d1735167c9ad07443ef07ff341d9ea1
GuillermoCochrane <guilleac81@gmail.com>
2025-07-04 19:13:10 -0300

    Componente modificado para renderizar componentes figure y code

--- src/components/partials/Answer/Answer.jsx
+++ src/components/partials/Answer/Answer.jsx
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types';
+import Figure from '../Figure/Figure';
+import Code from '../Code/Code';
 function Answer({Data}) {
   return (
           <details >
             <summary>{Data[0].alt}</summary>
             {
               Data.map ((item, index) => (
-                <figure key={index}>
-                  <img src={item.img} alt={item.alt} />
-                </figure>
+                <>
+                  {item.img && <Figure Data={item} key={`${index}-fig`} />}
+                  {item.language && <Code Data={item} key={`${index}-code`} />}
+                  {!item.language && !item.img && <p key={`${index}-para`}>Elemento no renderizable</p>}
+                </>
               ))
             }
           </details>
