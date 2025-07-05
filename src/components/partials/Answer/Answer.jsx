a99a9fde8705ff28334daf0b84026ea3c926b818
GuillermoCochrane <guilleac81@gmail.com>
2025-07-05 20:09:47 -0300

    corregido componente answers

--- src/components/partials/Answer/Answer.jsx
+++ src/components/partials/Answer/Answer.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
 import Figure from '../Figure/Figure';
 import Code from '../Code/Code';
+import { Fragment } from 'react';
 function Answer({Data}) {
   return (
           <details >
             <summary>{Data[0].alt}</summary>
             {
               Data.map ((item, index) => (
-                <>
-                  {item.img && <Figure Data={item} key={`${index}-fig`} />}
-                  {item.language && <Code Data={item} key={`${index}-code`} />}
-                  {!item.language && !item.img && <p key={`${index}-para`}>Elemento no renderizable</p>}
-                </>
+                <Fragment key={`${index}-fragment`}>
+                  {item.img && <Figure Data={item} />}
+                  {item.language && <Code Data={item} />}
+                  {!item.language && !item.img && <p>Elemento no renderizable</p>}
+                </Fragment>
               ))
             }
           </details>
