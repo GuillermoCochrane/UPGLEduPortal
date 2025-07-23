c4da78ae0d03b308d62b2fbb1e8cb11dd25c250a
GuillermoCochrane <guilleac81@gmail.com>
2025-07-23 18:10:54 -0300

    Modficada landing page

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -6,19 +6,23 @@ function Home() {
 
   return (
     <article className="home">
-        <img src={Logo} alt="Logo de UPGL" className="logo" />
-        <h1 className="UPGL" >Universidad Popular General Levalle</h1>
-        <h2>Seleccione su curso</h2>
         <section>
-        {
-          coursesIndex.map((item, index) => (
-            <h3 key={index}>
-              <Link to={item.links.link} key={index}>
-                {item.links.title}
-              </Link>
-            </h3>
-          ))
-        }
+          <img src={Logo} alt="Logo de UPGL" className="logo" />
+          <h1 className="UPGL" >Universidad Popular General Levalle</h1>
+        </section>
+        <section className="course-index">
+          <h2>Seleccione su curso</h2>
+          <nav>
+            {
+              coursesIndex.map((item, index) => (
+                <h3 key={index}>
+                  <Link to={item.links.link} key={index}>
+                    {item.links.title}
+                  </Link>
+                </h3>
+              ))
+            }
+          </nav>
         </section>
     </article>
   )
