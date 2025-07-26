c0ae7c68e89410760f003ca1f10295be34039e5d
GuillermoCochrane <guilleac81@gmail.com>
2025-07-26 14:40:54 -0300

    Agregado tema 1 de la clase 8

--- src/assets/data/FrontEnd.js
+++ src/assets/data/FrontEnd.js
@@ -9723,6 +9723,155 @@ button:hover {
       },
     ],
   },
+  {
+    class: 8,
+    topics: 7,
+    title: {
+      type: "h2",
+      info: [
+        {
+          text: "✨ Introducción a JavaScript para Desarrollo Web",
+          content: "plain",
+        },
+      ],
+    },
+    classData: [
+      {
+        topic: 1,
+        topicData: [
+          {
+            type: "h3",
+            info: [
+              {
+                text: "🎯 Objetivos de la clase",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "En esta clase vamos a introducir los fundamentos de JavaScript como lenguaje para el desarrollo web. Partiremos de su vínculo con el ",
+                content: "plain",
+              },
+              {
+                text: "HTML ",
+                content: "bold",
+              },
+              {
+                text: "a través del ",
+                content: "plain",
+              },
+              {
+                text: "DOM ",
+                content: "bold",
+              },
+              {
+                text: ", para luego conocer sus características principales y cómo se utiliza dentro de una página.",
+                content: "plain",
+              },
+            ],
+          },
+          {
+            type: "ul",
+            info: [
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Comprender qué es el ",
+                    content: "plain",
+                  },
+                  {
+                    text: "DOM ",
+                    content: "bold",
+                  },
+                  {
+                    text: "y cómo se relaciona con ",
+                    content: "plain",
+                  },
+                  {
+                    text: "JavaScript.",
+                    content: "bold",
+                  },
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Conocer el rol de ",
+                    content: "plain",
+                  },
+                  {
+                    text: "JavaScript ",
+                    content: "bold",
+                  },
+                  {
+                    text: "en el desarrollo frontend.",
+                    content: "plain",
+                  },
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Identificar formas de incluir ",
+                    content: "plain",
+                  },
+                  {
+                    text: "JavaScript ",
+                    content: "bold",
+                  },
+                  {
+                    text: "JavaScript en un documento ",
+                    content: "plain",
+                  },
+                  {
+                    text: "HTML.",
+                    content: "bold",
+                  },
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Reconocer los eventos básicos y cómo se capturan con ",
+                    content: "plain",
+                  },
+                  {
+                    text: "JavaScript.",
+                    content: "bold",
+                  }
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Aprender a declarar variables, constantes y funciones simples.",
+                    content: "plain",
+                  },
+                ],
+              },
+              {
+                type: "li",
+                info: [
+                  {
+                    text: "Poner en práctica estos conceptos mediante ejemplos concretos.",
+                    content: "plain",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  }
 ]
 
 
