5ad030eb80779258583f0f6707fdfcd111674c29
Guille <cochraneguillermo@gmail.com>
2026-04-19 19:00:27 -0300

    Agregado tema 4 de la clase 10

--- src/assets/data/FrontEnd.js
+++ src/assets/data/FrontEnd.js
@@ -13010,13 +13010,82 @@ cambiarColores();`,
         topic: 4,
         topicData: [
           {
-            type: "h2",
+            type: "h3",
             info: [
               {
-                text: "📚 Ejemplos prácticos con Bootstrap 5",
+                text: "🖥️ Ejemplos prácticos con Bootstrap 5",
                 content: "plain",
               },
             ],
+          },
+          {
+            type: "h4",
+            info: [
+              {
+                text: "🎨 Colores: ",
+                content: "plain",
+              },
+              {
+                text: "text-*",
+                content: "bold",
+              },
+              {
+                text: " y ",
+                content: "plain",
+              },
+              {
+                text: "bg-*",
+                content: "bold",
+              },
+            ],
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "🖌️ Colores de texto: ",
+                content: "bold",
+              },
+            ],
+          },
+          {
+            type: "example",
+            hasBT: true,
+            title: "🖌️ Colores de texto: ",
+            height: "140px",
+            info: [
+              {
+                type: "code",
+                info: {
+                  language: "html",
+                  content: `<div class="d-flex flex-wrap gap-2 align-items-center mx-auto my-1">\n  <p class="text-primary p-2 mx-4 my-2 border border-primary rounded-3">.text-primary</p>  \n  <p class="text-secondary p-2 mx-4 my-2 border border-secondary rounded-3">.text-secondary</p>  \n  <p class="text-success p-2 mx-4 my-2 border border-success rounded-3">.text-success</p>  \n  <p class="text-danger p-2 mx-4 my-2 border border-danger rounded-3">.text-danger</p>  \n  <p class="text-warning p-2 mx-4 my-2 border border-warning rounded-3">.text-warning</p>  \n  <p class="text-info p-2 mx-4 my-2 border border-info rounded-3">.text-info</p>  \n  <p class="text-light p-2 mx-4 my-2 border border-light bg-dark rounded-3">.text-light</p>  \n  <p class="text-dark p-2 mx-4 my-2 border border-dark rounded-3">.text-dark</p>  \n  <p class="text-body p-2 mx-4 my-2 border border-body rounded-3">.text-body</p>  \n  <p class="text-muted p-2 mx-4 my-2 border border-muted rounded-3">.text-muted</p>  \n </div>`
+                }
+              }
+            ]
+          },
+          {
+            type: "p",
+            info: [
+              {
+                text: "🎨 Colores de fondo: ",
+                content: "bold",
+              },
+            ],
+          },
+          {
+            type: "example",
+            hasBT: true,
+            title: "🎨 Colores de fondo: ",
+            height: "140px",
+            info: [
+              {
+                type: "code",
+                info: { 
+                language: "html",
+                content: `<div class="d-flex flex-wrap gap-2 align-items-center mx-auto my-1">\n  <div class="p-2 mx-4 my-2 bg-primary text-white rounded">.bg-primary</div>  \n  <div class="p-2 mx-4 my-2 bg-secondary text-white rounded">.bg-secondary</div>  \n  <div class="p-2 mx-4 my-2 bg-success text-white rounded">.bg-success</div>  \n  <div class="p-2 mx-4 my-2 bg-danger text-white rounded">.bg-danger</div>  \n  <div class="p-2 mx-4 my-2 bg-warning text-white rounded">.bg-warning</div>  \n  <div class="p-2 mx-4 my-2 bg-info text-white rounded">.bg-info</div>  \n  <div class="p-2 mx-4 my-2 bg-light text-dark rounded">.bg-light</div>  \n  <div class="p-2 mx-4 my-2 bg-dark text-white rounded">.bg-dark</div>  \n  <div class="p-2 mx-4 my-2 bg-body text-dark rounded">.bg-body</div>  \n  <div class="p-2 mx-4 my-2 bg-muted text-dark rounded">.bg-muted</div>  \n </div>`
+                }
+              }
+            ]
           },
         ]
       },
@@ -13067,11 +13136,3 @@ cambiarColores();`,
 ]
 
 export default classes;
-
-          /* {
-            type: "code",
-            info: {
-              language: "html",
-              content: `<html>\n  <body>\n    <h1>Hola</h1>\n    <p id="parrafo">Texto</p>\n  </body>\n</html>`
-            }
-          }, */
