1169b09ae88ffd257db7870b1ba44cb044f2ed2f
Guille <cochraneguillermo@gmail.com>
2026-05-19 22:22:08 -0300

    Agregado tema 2 de la clase 1 del curso de backend

--- src/assets/data/BackEnd.js
+++ src/assets/data/BackEnd.js
@@ -44,6 +44,50 @@ const classes = [
           },
         ],
       },
+      {
+        topic: 2,
+        topicData: [
+          {
+            type: "h3",
+            info: [
+              { text: " 💻 Programa del curso 💻", content: "plain" },
+            ],
+          },
+          {
+            type: "figure",
+            info: {
+              img: "/images/Backend/class1/programa.webp",
+              alt: "Programa del curso",
+            },
+          },
+          {
+            type: "h3",
+            info: [
+              { text: "🔎 Detalles 🔎", content: "plain" },
+            ],
+          },
+          {
+            type: "figure",
+            info: {
+              img: "/images/Backend/class1/programa_explicado.webp",
+              alt: "Descubre el programa 🔎",
+            },
+          },
+          {
+            type: "h3",
+            info: [
+              { text: "💡 Proyectos que vamos a desarrollar 💡", content: "plain" },
+            ],
+          },
+          {
+            type: "figure",
+            info: {
+              img: "/images/Backend/class1/programa_proyectos.webp",
+              alt: "Proyectos 💡",
+            },
+          },
+        ],
+      },
     ],
   },
 ];
