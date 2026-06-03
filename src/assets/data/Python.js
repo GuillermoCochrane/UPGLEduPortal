dd5a1fd26abfc47f36ec9f0c4d1cc342611e4356
Guille <cochraneguillermo@gmail.com>
2026-06-03 03:12:12 -0300

    Imagen del metodo 3, del ejemplo 2, del tema 2, de la clase 11 reemplazada por Code

--- src/assets/data/Python.js
+++ src/assets/data/Python.js
@@ -11172,10 +11172,10 @@ let classes = [
             ],
           },
           {
-            type: "figure",
+            type: "code",
             info: {
-              img: "/images/Python/class11/generador_de_cuadrados_desempaquetado.png",
-              alt: "Generador de Cuadrados usando print con desempaquetado",
+              language: "python",
+              content: "# Usar la función print con desempaquetado\ncuadrados = (x**2 for x in range(10))\nprint(*cuadrados)\n# Imprime: 0 1 4 9 16 25 36 49 64 81",
             }
           },
           {
