cb7a0d9e2516df27794ba9da25239ba2406fe5b5
Guille <cochraneguillermo@gmail.com>
2026-06-25 17:47:24 -0300

    Componente Answer modificado para soportar Example, y reeulta deuda tecnica de la adaptacion anterior

--- src/components/partials/Answer/Answer.jsx
+++ src/components/partials/Answer/Answer.jsx
@@ -1,26 +1,28 @@
 import PropTypes from 'prop-types';
 import Figure from '../Figure/Figure';
 import Code from '../Code/Code';
+import Example from '../Example/Example';
 import { Fragment } from 'react';
-function Answer({Data}) {
+function Answer({ Data }) {
   return (
-          <details >
-            <summary>{Data[0].alt}</summary>
-            {
-              Data.map ((item, index) => (
-                <Fragment key={`${index}-fragment`}>
-                  {item.img && <Figure Data={item} />}
-                  {item.language && <Code Data={item} />}
-                  {!item.language && !item.img && <p>Elemento no renderizable</p>}
-                </Fragment>
-              ))
-            }
-          </details>
+    <details >
+      <summary>{Data.title}</summary>
+      {
+        Data.content.map((item, index) => (
+          <Fragment key={`${index}-fragment`}>
+            {item.type === "figure" && <Figure Data={item.info} />}
+            {item.type === "code" && <Code Data={item.info} />}
+            {item.type === "example" && <Example Data={item.info} height={item.height} width={item.width} title={item.title} hasBT={item.hasBT} />}
+            {item.type !== "figure" && item.type !== "code" && item.type !== "example" && <p>Elemento no renderizable</p>}
+          </Fragment>
+        ))
+      }
+    </details>
   );
 }
 
 Answer.propTypes = {
-  Data: PropTypes.array.isRequired,
+  Data: PropTypes.object.isRequired,
 }
 
 export default Answer;
