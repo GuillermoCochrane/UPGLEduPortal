072694ba02bc10b579f8dbd95dc9df8fa2c48dff
Guille <cochraneguillermo@gmail.com>
2026-06-26 20:42:04 -0300

    Modficada Solucion de la actividad 2 del tema 4 de la clase 4 del curso de python, por el refactor de answer

--- src/assets/data/Python.js
+++ src/assets/data/Python.js
@@ -4738,13 +4738,18 @@ let classes = [
           },
           {
             type: "answer",
-            info: [
-              {
-                language: "python",
-                content: `# Escribir 3 variables para que guarden Apellido y nombre - Email y telefono y \n# mostrarlos en pantalla, con el siguiente mensaje "Hola, Mi nombre es: ..., \n# te paso mi contacto telefonico: ... y mi email: ..."\n\n# - Pedirle al usuario que ponga como estuvo su dia (del 1 al 10) y luego \n# mostrar el texto "Mi dia estuvo de: " y el valor ingresado\n# - Pedir al usuario que ingrese su edad y su ciudad de residencia, y luego \n# mostrar estos datos en una frase.\n# - Solicitar al usuario que ingrese su comida favorita y su bebida favorita, \n# luego mostrar un mensaje que contenga ambas respuestas.\n\nnombreApellido = "Walter White"\ntelefono = "+1 (505) 555-0199"\nmail = "walter.white@jpwynnehs.edu"\n\nprint("Hola, mi nombre es", nombreApellido, "y te paso mi contacto telefonico", \ntelefono, "y mi email", mail)\n\ndia = int(input("¿Como estuvo de su día? "))\nprint(f"Mi día estuvo de {dia}")\n\nedad = int(input("¿Cuántos años tiene? "))\nciudad = input("¿Cuál es tu ciudad de residencia? ")\nprint(f"Mi edad es {edad} y vivo en {ciudad}")\n\ncomida = input("Cual es tu comida favorita? ")\nbebida = input("Cual es tu bebida favorita? ")\nprint(f"Mi comida favorita es {comida} y mi bebida favorita es {bebida}")`,
-                alt: "Solución ejercicio 2",
-              },
-            ],
+            info: {
+              title: "Solución ejercicio 2",
+              content: [
+                {
+                  type: "code",
+                  info: {
+                    language: "python",
+                    content: "# Escribir 3 variables para que guarden Apellido y nombre -\n# Email y telefono y mostrarlos en pantalla, con el siguiente\n# mensaje \"Hola, Mi nombre es: ..., te paso mi contacto\n# telefonico: ... y mi email: ...\"\n#\n# - Pedirle al usuario que ponga como estuvo su dia (del 1 al 10)\n# y luego mostrar el texto \"Mi dia estuvo de: \" y el valor ingresado\n# - Pedir al usuario que ingrese su edad y su ciudad de\n# residencia, y luego mostrar estos datos en una frase.\n# - Solicitar al usuario que ingrese su comida favorita y su bebida\n# favorita, luego mostrar un mensaje que contenga ambas respuestas.\n\nnombreApellido = \"Walter White\"\ntelefono = \"+1 (505) 555-0199\"\nmail = \"walter.white@jpwynnehs.edu\"\n\n# Fraccionamos el print para evitar desbordamientos\nprint(\n    \"Hola, mi nombre es\",\n    nombreApellido,\n    \"y te paso mi contacto telefonico\",\n    telefono,\n    \"y mi email\",\n    mail,\n)\n\ndia = int(input(\"¿Como estuvo de su día? \"))\nprint(f\"Mi día estuvo de {dia}\")\n\nedad = int(input(\"¿Cuántos años tiene? \"))\nciudad = input(\"¿Cuál es tu ciudad de residencia? \")\nprint(f\"Mi edad es {edad} y vivo en {ciudad}\")\n\ncomida = input(\"Cual es tu comida favorita? \")\nbebida = input(\"Cual es tu bebida favorita? \")\nprint(f\"Mi comida favorita es {comida} y mi bebida favorita es {bebida}\")",
+                  },
+                },
+              ],
+            },
           },
         ],
       },
