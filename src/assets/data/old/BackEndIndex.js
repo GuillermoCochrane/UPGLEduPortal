92102ea9ea70b2dcfe3c78d0ef6fd7481cca9987
Guille <cochraneguillermo@gmail.com>
2026-07-02 17:19:35 -0300

    Backup de DB del curso de backend previo a la modularizacion

---
+++ src/assets/data/old/BackEndIndex.js
@@ -0,0 +1,35 @@
+let classIndex = [
+  {
+    summary: "Introducción",
+    classId: 1,
+    links: [
+      {
+        title: "Introducción y Repaso",
+        topicId: 1,
+        link: "/courses/Backend/class/1/1"
+      },
+      {
+        title: "Programa del curso",
+        topicId: 2,
+        link: "/courses/Backend/class/1/2"
+      },
+      {
+        title: "¿Cómo funciona una página web?",
+        topicId: 3,
+        link: "/courses/Backend/class/1/3"
+      },
+      {
+        title: "¿Cómo funciona internet?",
+        topicId: 4,
+        link: "/courses/Backend/class/1/4"
+      },
+      {
+        title: "Bonus track",
+        topicId: 5,
+        link: "/courses/Backend/class/1/5"
+      }
+    ]
+  },
+]
+
+export default  classIndex
