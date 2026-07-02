eefdb04f2416b75b31ef90224ba8db48cb6fd9c1
Guille <cochraneguillermo@gmail.com>
2026-07-02 17:19:00 -0300

    Backup de DB del curso de frontend previo a la modularizacion

---
+++ src/assets/data/old/FrontendIndex.js
@@ -0,0 +1,436 @@
+let classIndex = [
+  {
+    summary: "Introducción a la Web y HTML Básico",
+    classId: 1,
+    links: [
+      {
+        title: "¿Qué es la programación?",
+        topicId: 1,
+        link: "/courses/Frontend/class/1/1"
+      },
+      {
+        title: "¿Qué es un programador?",
+        topicId: 2,
+        link: "/courses/Frontend/class/1/2"
+      },
+      {
+        title: "Historia y evolución de la web.",
+        topicId: 3,
+        link: "/courses/Frontend/class/1/3"
+      },
+      {
+        title: "Frontend vs Backend",
+        topicId: 4,
+        link: "/courses/Frontend/class/1/4"
+      },
+      {
+        title: "Entorno de Trabajo",
+        topicId: 5,
+        link: "/courses/Frontend/class/1/5"
+      },
+      {
+        title: "Introducción a HTML Básico",
+        topicId: 6,
+        link: "/courses/Frontend/class/1/6"
+      },
+      {
+        title: "Actividades",
+        topicId: 7,
+        link: "/courses/Frontend/class/1/7"
+      },
+    ]
+  },
+  {
+    summary: "Estructura HTML, Enlaces y Formularios",
+    classId: 2,
+    links: [
+      {
+        title: "Objetivos de la clase",
+        topicId: 1,
+        link: "/courses/Frontend/class/2/1"
+      },
+      {
+        title: "¿Cómo funcionan las etiquetas HTML?",
+        topicId: 2,
+        link: "/courses/Frontend/class/2/2"
+      },
+      {
+        title: "¿Qué son los formularios?",
+        topicId: 3,
+        link: "/courses/Frontend/class/2/3"
+      },
+      {
+        title: "¿Qué son las listas?",
+        topicId: 4,
+        link: "/courses/Frontend/class/2/4"
+      },
+      {
+        title: "Actividades",
+        topicId: 5,
+        link: "/courses/Frontend/class/2/5"
+      },
+    ]
+  }, 
+  {
+    summary: "Tablas e Imagenes",
+    classId: 3,
+    links: [
+      {
+        title: "Objetivos de la clase",
+        topicId: 1,
+        link: "/courses/Frontend/class/3/1"
+      },
+      {
+        title: "Tablas",
+        topicId: 2,
+        link: "/courses/Frontend/class/3/2"
+      },
+      {
+        title: "Manejo de imágenes",
+        topicId: 3,
+        link: "/courses/Frontend/class/3/3"
+      },
+      {
+        title: "Actividades",
+        topicId: 4,
+        link: "/courses/Frontend/class/3/4"
+      },
+    ]
+  }, 
+  {
+    summary: "Tipos de Inputs",
+    classId: 4,
+    links: [
+      {
+        title: "Tipos de Inputs en HTML",
+        topicId: 1,
+        link: "/courses/Frontend/class/4/1"
+      },
+      {
+        title: "Inputs Numéricos y de Fecha/Hora",
+        topicId: 2,
+        link: "/courses/Frontend/class/4/2"
+      },
+      {
+        title: "Inputs de Selección",
+        topicId: 3,
+        link: "/courses/Frontend/class/4/3"
+      },
+      {
+        title: "Inputs de Botones",
+        topicId: 4,
+        link: "/courses/Frontend/class/4/4"
+      },
+      {
+        title: "Inputs Especiales",
+        topicId: 5,
+        link: "/courses/Frontend/class/4/5"
+      },
+      {
+        title: "Atributos importantes para inputs",
+        topicId: 6,
+        link: "/courses/Frontend/class/4/6"
+      },
+      {
+        title: "Buenas Prácticas para Inputs",
+        topicId: 7,
+        link: "/courses/Frontend/class/4/7" 
+      },
+      {
+        title: "Actividades",
+        topicId: 8,
+        link: "/courses/Frontend/class/4/8"
+      }
+    ]
+  }, 
+  {
+    summary: "Semántica HTML y Fundamentos CSS",
+    classId: 5,
+    links: [
+      {
+        title: "Etiquetas semánticas",
+        topicId: 1,
+        link: "/courses/Frontend/class/5/1"
+      },
+      {
+        title: "Introducción a CSS – Fundamentos",
+        topicId: 2,
+        link: "/courses/Frontend/class/5/2"
+      },
+      {
+        title: "Selectores y estilización básica",
+        topicId: 3,
+        link: "/courses/Frontend/class/5/3"
+      },
+      {
+        title: "Actividades",
+        topicId: 4,
+        link: "/courses/Frontend/class/5/4"
+      },
+    ]
+  },
+  {
+    summary: "Repaso de CSS, selectores y unidades",
+    classId: 6,
+    links: [
+      {
+        title: "Repaso de CSS",
+        topicId: 1,
+        link: "/courses/Frontend/class/6/1"
+      },
+      {
+        title: "Cheatsheet de Propiedades CSS",
+        topicId: 2,
+        link: "/courses/Frontend/class/6/2"
+      },
+      {
+        title: "Tipos de Selectores en CSS",
+        topicId: 3,
+        link: "/courses/Frontend/class/6/3"
+      },
+      {
+        title: "Unidades de CSS",
+        topicId: 4,
+        link: "/courses/Frontend/class/6/4"
+      },
+      {
+        title: "Actividades",
+        topicId: 5,
+        link: "/courses/Frontend/class/6/5"
+      },
+    ]
+  },
+  {
+    summary: "Animaciones, Responsiveness y UI",
+    classId: 7,
+    links: [
+      {
+        title: "Objetivos de la clase",
+        topicId: 1,
+        link: "/courses/Frontend/class/7/1"
+      },
+      {
+        title: "Animaciones en CSS",
+        topicId: 2,
+        link: "/courses/Frontend/class/7/2"
+      },
+      {
+        title: "Diseño Responsive",
+        topicId: 3,
+        link: "/courses/Frontend/class/7/3"
+      },
+      {
+        title: "UI Moderna",
+        topicId: 4,
+        link: "/courses/Frontend/class/7/4"
+      },
+      {
+        title: "Bonus: Webs con recursos CSS",
+        topicId: 5,
+        link: "/courses/Frontend/class/7/5"
+      },
+      {
+        title: "Actividades",
+        topicId: 6,
+        link: "/courses/Frontend/class/7/6"
+      }
+    ]
+  },
+  {
+    summary: "JavaScript para Desarrollo Web",
+    classId: 8,
+    links: [
+      {
+        title: "Objetivos de la clase",
+        topicId: 1,
+        link: "/courses/Frontend/class/8/1"
+      },
+      {
+        title: "¿Qué es el DOM?",
+        topicId: 2,
+        link: "/courses/Frontend/class/8/2"
+      },
+      {
+        title: "¿Qué es JavaScript?",
+        topicId: 3,
+        link: "/courses/Frontend/class/8/3"
+      },
+      {
+        title: "¿Como se usa JavaScript?",
+        topicId: 4,
+        link: "/courses/Frontend/class/8/4"
+      },
+      {
+        title: "Eventos en JavaScript",
+        topicId: 5,
+        link: "/courses/Frontend/class/8/5"
+      },
+      {
+        title: "Variables, Constantes y Funciones",
+        topicId: 6,
+        link: "/courses/Frontend/class/8/6"
+      },
+      {
+        title: "Actividades",
+        topicId: 7,
+        link: "/courses/Frontend/class/8/7"
+      },
+    ]
+  },
+  {
+    summary: "Tipos de Datos y Estructuras",
+    classId: 9,
+    links: [
+      {
+        title: "Tipos de Datos en JavaScript",
+        topicId: 1,
+        link: "/courses/Frontend/class/9/1"
+      },
+      {
+        title: "Variables y Constantes",
+        topicId: 2,
+        link: "/courses/Frontend/class/9/2"
+      },
+      {
+        title: "Funciones",
+        topicId: 3,
+        link: "/courses/Frontend/class/9/3"
+      },
+      {
+        title: "Estructuras de Control",
+        topicId: 4,
+        link: "/courses/Frontend/class/9/4"
+      },
+      {
+        title: "Funciones útiles para el DOM",
+        topicId: 5,
+        link: "/courses/Frontend/class/9/5"
+      },
+      {
+        title: "Actividades",
+        topicId: 6,
+        link: "/courses/Frontend/class/9/6"
+      },
+    ]
+  },
+  {
+    summary: "Introducción a Bootstrap",
+    classId: 10,
+    links: [
+      {
+        title: "¿Qué es Bootstrap?",
+        topicId: 1,
+        link: "/courses/Frontend/class/10/1"
+      },
+      {
+        title: "Historia y evolución",
+        topicId: 2,
+        link: "/courses/Frontend/class/10/2"
+      },
+      {
+        title: "¿Cómo se usa Bootstrap?",
+        topicId: 3,
+        link: "/courses/Frontend/class/10/3"
+      },
+      {
+        title: "Ejemplos de colores",
+        topicId: 4,
+        link: "/courses/Frontend/class/10/4"
+      },
+      {
+        title: "Ejemplos de texto",
+        topicId: 5,
+        link: "/courses/Frontend/class/10/5"
+      },
+      {
+        title: "Ejemplos de botones",
+        topicId: 6,
+        link: "/courses/Frontend/class/10/6"
+      },
+      {
+        title: "Ejemplos de formularios",
+        topicId: 7,
+        link: "/courses/Frontend/class/10/7"
+      },
+      {
+        title: "Ejemplos de tablas",
+        topicId: 8,
+        link: "/courses/Frontend/class/10/8"
+      },
+    ]
+  },
+  {
+    summary: "Bootstrap avanzado",
+    classId: 11,
+    links: [
+      {
+        title: "Introducción a Bootstrap",
+        topicId: 1,
+        link: "/courses/Frontend/class/11/1"
+      },
+      {
+        title: "Sistema de Grillas",
+        topicId: 2,
+        link: "/courses/Frontend/class/11/2"
+      },
+      {
+        title: "Botones",
+        topicId: 3,
+        link: "/courses/Frontend/class/11/3"
+      },
+      {
+        title: "Tablas y Formularios",
+        topicId: 4,
+        link: "/courses/Frontend/class/11/4"
+      },
+      {
+        title: "Cards",
+        topicId: 5,
+        link: "/courses/Frontend/class/11/5"
+      },
+      {
+        title: "Modales Básicos",
+        topicId: 6,
+        link: "/courses/Frontend/class/11/6"
+      },
+      {
+        title: "Actividades",
+        topicId: 7,
+        link: "/courses/Frontend/class/11/7"
+      },
+    ]
+  },
+  {
+    summary: "Fetch API",
+    classId: 12,
+    links: [
+      {
+        title: "¿Qué es Fetch?",
+        topicId: 1,
+        link: "/courses/Frontend/class/12/1"
+      },
+      {
+        title: "Como se usa Fetch",
+        topicId: 2,
+        link: "/courses/Frontend/class/12/2"
+      },
+      {
+        title: "Manejo de errores",
+        topicId: 3,
+        link: "/courses/Frontend/class/12/3"
+      },
+      {
+        title: "Ejemplo práctico",
+        topicId: 4,
+        link: "/courses/Frontend/class/12/4"
+      },
+      {
+        title: "Actividades",
+        topicId: 5,
+        link: "/courses/Frontend/class/12/5"
+      },
+    ]
+  },
+]
+
+export default  classIndex
