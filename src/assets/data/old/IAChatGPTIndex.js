bfcccb68a55c874fd222916c7b7fc9a06b116033
Guille <cochraneguillermo@gmail.com>
2026-07-02 17:21:30 -0300

    Backup de DB del curso de IA previo a la modularizacion

---
+++ src/assets/data/old/IAChatGPTIndex.js
@@ -0,0 +1,189 @@
+let classIndex = [
+    {
+      summary: "Conceptos fundamentales de IA",
+      classId: 1,
+      links: [
+        {
+          title: "¿Qué es la inteligencia?",
+          topicId: 1,
+          link: "/courses/IA/class/1/1"
+        },
+        {
+          title: "¿Qué es la inteligencia artificial?",
+          topicId: 2,
+          link: "/courses/IA/class/1/2"
+        },
+        {
+          title: "Diferencias entre Inteligencia Artificial e Inteligencia Natural",
+          topicId: 3,
+          link: "/courses/IA/class/1/3"
+        }
+      ]
+    },
+    { 
+      summary: "Historia de la inteligencia artificial",
+      classId: 2,
+      links: [
+        {
+          title: "Década de 1950",
+          topicId: 1,
+          link: "/courses/IA/class/2/1"
+        },
+        {
+          title: "Década de 1960-1970",
+          topicId: 2,
+          link: "/courses/IA/class/2/2"
+        },
+        {
+          title: "Década de 1980",
+          topicId: 3,
+          link: "/courses/IA/class/2/3"
+        },
+        {
+          title: "Década de 1990",
+          topicId: 4,
+          link: "/courses/IA/class/2/4"
+        },
+        {
+          title: "Década de 2000",
+          topicId: 5,
+          link: "/courses/IA/class/2/5"
+        },
+        {
+          title: "Década de 2010",
+          topicId: 6,
+          link: "/courses/IA/class/2/6"
+        },
+        {
+          title: "Actualidad",
+          topicId: 7,
+          link: "/courses/IA/class/2/7"
+        }
+      ]   
+    },
+    {
+      summary: "Machine Learning y Redes Neuronales",
+      classId: 3,
+      links: [
+        {
+          title: "¿Qué es y cómo funciona el Machine Learning?",
+          topicId: 1,
+          link: "/courses/IA/class/3/1"
+        },
+        {
+          title: "¿Qué son y cómo funcionan las Redes Neuronales?",
+          topicId: 2,
+          link: "/courses/IA/class/3/2"
+        }
+      ]
+    },
+    {
+      summary: "Origen de ChatGPT y sus versiones",
+      classId: 4,
+      links: [
+        {
+          title: "Origen",
+          topicId: 1,
+          link: "/courses/IA/class/4/1"
+        },
+        {
+          title: "Todas sus versiones",
+          topicId: 2,
+          link: "/courses/IA/class/4/2"
+        }
+      ]
+    },
+    {
+      summary: "Prompt: definición y conceptos",
+      classId: 5,
+      links: [
+        {
+          title: "¿Qué es un Prompt?",
+          topicId: 1,
+          link: "/courses/IA/class/5/1"
+        },
+        {
+          title: "Componentes de un Prompt",
+          topicId: 2,
+          link: "/courses/IA/class/5/2"
+        },
+        {
+          title: "Aplicación de Prompt en tareas especificas",
+          topicId: 3,
+          link: "/courses/IA/class/5/3"
+        }
+      ]
+    },
+    {
+      summary: "Diseño Efectivo de Prompts",
+      classId: 6,
+      links: [
+        {
+          title: "Diseño",
+          topicId: 1,
+          link: "/courses/IA/class/6/1"
+        },
+        {
+          title: "¿Cómo dar instrucciones claras y precisas?",
+          topicId: 2,
+          link: "/courses/IA/class/6/2"
+        },
+        {
+          title: "Ejemplos y mejores practicas",
+          topicId: 3,
+          link: "/courses/IA/class/6/3"
+        }
+      ]
+    },
+    {
+      summary: "Ingeniería de Prompts",
+      classId: 7,
+      links: [
+        {
+          title: "Pasos para la optimización de un Prompt",
+          topicId: 1,
+          link: "/courses/IA/class/7/1"
+        }
+      ]
+    },
+    {
+      summary: "Formatos de salida",
+      classId: 8,
+      links: [
+        {
+          title: "Formatos de salida",
+          topicId: 1,
+          link: "/courses/IA/class/8/1"
+        },
+        {
+          title: "Evaluación de Prompts",
+          topicId: 2,
+          link: "/courses/IA/class/8/2"
+        }
+      ]
+    },
+    {
+      summary: "Estrategias para el uso de ChatGPT",
+      classId: 9,
+      links: [
+        {
+          title: "Estrategias para mantener conversaciones con ChatGPT",
+          topicId: 1,
+          link: "/courses/IA/class/9/1"
+        }
+      ]
+    },
+    {
+      summary: "¿En qué casos usar ChatGPT?",
+      classId: 10,
+      links: [
+        {
+          title: "¿En qué casos usar ChatGPT?",
+          topicId: 1,
+          link: "/courses/IA/class/10/1"
+        }
+      ]
+    }   
+  ]
+
+export default  classIndex
