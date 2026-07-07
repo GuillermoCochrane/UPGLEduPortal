32180da43952c92cb0ed68dd5862d0b6da72be75
Guille <cochraneguillermo@gmail.com>
2026-07-07 19:03:47 -0300

    Indice de la clase 9 del curso de IA y todos sus temas, incluidos en la nueva estructura de datos

--- src/assets/data/ia/class-9.js
+++ src/assets/data/ia/class-9.js
@@ -2,6 +2,8 @@ import { topic1 } from "./class-9/topic-1.js";
 
 export const class9 = {
   class: 9,
+  summary: "Estrategias para el uso de ChatGPT",
+  available: true,
   topics: 1,
   title: {
     type: "h2",
--- src/assets/data/ia/class-9/topic-1.js
+++ src/assets/data/ia/class-9/topic-1.js
@@ -2,6 +2,8 @@ import { item1, item2, item3, item4 } from "./topic-1/data.js";
 
 export const topic1 = {
   topic: 1,
+  title: "Estrategias para mantener conversaciones con ChatGPT",
+  available: true,
   topicData: [
     item1,
     item2,
